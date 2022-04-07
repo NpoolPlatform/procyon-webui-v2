225207a420859dc5fb866a6f37e843144542a0e2
hong-t <th13564039482@163.com>
2022-04-07 19:34:47 +0800

    add whyprocyon

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,7 @@
   <div class='content'>
     <Banner />
     <Products />
+    <Whyprocyon />
     <div class='hr' />
   </div>
 </template>
@@ -11,5 +12,6 @@ import { defineAsyncComponent } from 'vue'
 
 const Banner = defineAsyncComponent(() => import('src/components/index/Banner.vue'))
 const Products = defineAsyncComponent(() => import('src/components/index/Products.vue'))
+const Whyprocyon = defineAsyncComponent(() => import('src/components/index/Whyprocyon.vue'))
 
 </script>
