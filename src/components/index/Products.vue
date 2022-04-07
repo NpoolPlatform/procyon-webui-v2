b518ee98e18e553feda94c053d2da0d51dc6e699
Zhao KK <kikakkz@hotmail.com>
2022-04-07 15:52:26 +0800

    Add product card

--- src/components/index/Products.vue
+++ src/components/index/Products.vue
@@ -1,7 +1,67 @@
 <template>
   <h2>{{ $t('MSG_PREMIERE_PRODUCTS') }}</h2>
+  <div class='products'>
+    <Product v-for='myGood in goods' :key='myGood.Good.Good.ID' :good='myGood' />
+  </div>
 </template>
 
 <script setup lang='ts'>
+import { computed, defineAsyncComponent, onMounted } from 'vue'
+import { useGoodStore, NotificationType } from 'npool-cli-v2'
+import { useI18n } from 'vue-i18n'
+
+// eslint-disable-next-line @typescript-eslint/unbound-method
+const { t } = useI18n({ useScope: 'global' })
+
+const Product = defineAsyncComponent(() => import('src/components/product/Card.vue'))
+
+const good = useGoodStore()
+const goods = computed(() => good.getRecommendGoods)
+
+onMounted(() => {
+  if (goods.value.length > 0) {
+    return
+  }
+
+  good.getGoods({
+    Message: {
+      Error: {
+        Title: t('MSG_GET_GOODS_FAIL'),
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  })
+
+  good.getRecommends({
+    Message: {
+      Error: {
+        Title: t('MSG_GET_RECOMMENDS_FAIL'),
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  })
+
+  good.getPromotions({
+    Message: {
+      Error: {
+        Title: t('MSG_GET_PROMOTIONS_FAIL'),
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  })
+
+  good.getAppGoods({
+    Message: {
+      Error: {
+        Title: t('MSG_GET_APP_GOODS_FAIL'),
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  })
+})
 
 </script>
---
+++ src/components/product/Card.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class='product'>
+    <div class='product-heading'>
+      <img class='icon' src='product-aleo.png'>
+      <h3 class='product-title'>
+        {{ good.Good.Good.Title }}
+      </h3>
+    </div>
+    <h4 class='price'>
+      <span>{{ goods.getGoodPrice(good) }}</span> USDT / {{ good.Good.Good.Unit }}
+    </h4>
+    <div class='line'>
+      <span class='label'>{{ $t('MSG_DAILY_MINING_REWARDS') }}:</span>
+      <span class='value'>*{{ good.Main?.Unit }} / {{ $t('MSG_DAY') }}</span>
+    </div>
+
+    <div class='line'>
+      <span class='label'>{{ $t('MSG_SERVICE_PERIOD') }}:</span>
+      <span class='value'>{{ good.Good.Good.DurationDays }} {{ $t('MSG_DAYS') }}</span>
+    </div>
+
+    <div class='line'>
+      <span class='label'>{{ $t('MSG_TECHNIQUE_SERVICE_FEE') }}:</span>
+      <span class='value'>20%</span>
+    </div>
+
+    <div class='line'>
+      <span class='label'>{{ $t('MSG_MAINTENANCE_FEE') }}:</span>
+      <span class='value'>$0.05 / {{ $t('MSG_DAY') }}</span>
+    </div>
+
+    <div class='line'>
+      <span class='label'>{{ $t('MSG_ORDER_EFFECTIVE') }}:</span>
+      <span class='value'>{{ formatTime(good.Good.Good.StartAt) }}</span>
+    </div>
+    <button class='alt'>
+      {{ $t('MSG_PURCHASE') }}
+    </button>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { defineProps, toRef } from 'vue'
+import { Good, useGoodStore, formatTime } from 'npool-cli-v2'
+
+interface Props {
+  good: Good
+}
+
+const props = defineProps<Props>()
+const good = toRef(props, 'good')
+
+const goods = useGoodStore()
+
+</script>
