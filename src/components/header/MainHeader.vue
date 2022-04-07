e9528261ca744d6a54e048bf87dc2eb4ccac434f
Zhao KK <kikakkz@hotmail.com>
2022-04-07 13:33:49 +0800

    Add navigation target

--- src/components/header/MainHeader.vue
+++ src/components/header/MainHeader.vue
@@ -3,10 +3,10 @@
     <img :src='lightLogo' class='attachment-large size-large logo'>
     <div class='nav'>
       <ul>
-        <li><a class='nav-link' href=''>{{ $t('MSG_HOME') }}</a></li>
-        <li><a class='nav-link' href=''>{{ $t('MSG_BLOG') }}</a></li>
-        <li><a class='nav-link' href=''>{{ $t('MSG_SUPPORT_AND_FAQ') }}</a></li>
-        <li><a class='nav-link' href=''>{{ $t('MSG_CONTACT') }}</a></li>
+        <li><a class='nav-link' href='#'>{{ $t('MSG_HOME') }}</a></li>
+        <li><a class='nav-link' href='/blog'>{{ $t('MSG_BLOG') }}</a></li>
+        <li><a class='nav-link' href='/faq'>{{ $t('MSG_SUPPORT_AND_FAQ') }}</a></li>
+        <li><a class='nav-link' href='/contact'>{{ $t('MSG_CONTACT') }}</a></li>
         <ul class='language-picker'>
           <li class='selected'>
             <a class='language'>EN</a>
@@ -47,10 +47,10 @@
     <div class='hr' />
     <div class='nav'>
       <ul>
-        <li><a class='nav-link' href=''>{{ $t('MSG_HOME') }}</a></li>
-        <li><a class='nav-link' href=''>{{ $t('MSG_BLOG') }}</a></li>
-        <li><a class='nav-link' href=''>{{ $t('MSG_SUPPORT_AND_FAQ') }}</a></li>
-        <li><a class='nav-link' href=''>{{ $t('MSG_CONTACT') }}</a></li>
+        <li><a class='nav-link' href='#'>{{ $t('MSG_HOME') }}</a></li>
+        <li><a class='nav-link' href='/blog'>{{ $t('MSG_BLOG') }}</a></li>
+        <li><a class='nav-link' href='/faq'>{{ $t('MSG_SUPPORT_AND_FAQ') }}</a></li>
+        <li><a class='nav-link' href='/contact'>{{ $t('MSG_CONTACT') }}</a></li>
       </ul>
     </div>
   </header>
