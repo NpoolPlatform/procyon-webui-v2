8bb307cdcd3e759346f16187719d6f9fd5898f8a
Zhao KK <kikakkz@hotmail.com>
2022-04-08 15:54:46 +0800

    Add side bar menu

--- src/components/menu/SideMenu.vue
+++ src/components/menu/SideMenu.vue
@@ -2,7 +2,7 @@
   <div class='sidebar' v-if='setting.ShowSideMenu'>
     <img :src='lightLogo' class='attachment-large size-large logo' @click='onLogoClick'>
     <ul>
-      <li v-for='menu in BaseMenu.children' :key='menu.menuId'>
+      <li v-for='menu in BaseMenu.children' :key='menu.menuId' @click='onMenuSwitch(menu)'>
         <q-icon :name='menu.icon' /><span>{{ $t(menu.label) }}</span>
       </li>
     </ul>
@@ -12,7 +12,7 @@
 <script setup lang='ts'>
 import { useSettingStore } from '../../store/setting'
 import { useRouter } from 'vue-router'
-import { BaseMenu } from 'src/menus/menus'
+import { BaseMenu, MenuItem } from 'src/menus/menus'
 
 import lightLogo from '../../assets/procyon-light.svg'
 
@@ -21,6 +21,10 @@ const setting = useSettingStore()
 const router = useRouter()
 const onLogoClick = () => {
   void router.push({ path: '/' })
+}
+
+const onMenuSwitch = (menu: MenuItem) => {
+  void router.push({ path: menu.target })
 }
 
 </script>
