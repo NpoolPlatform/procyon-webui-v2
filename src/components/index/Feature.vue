c91ed314f6e0099e7d2b3963bcc4ae67cffe7544
hong-t <th13564039482@163.com>
2022-04-09 22:03:59 +0800

    add Feature.vue

---
+++ src/components/index/Feature.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class='feature'>
+    <div class='content'>
+      <div class='columns-3'>
+        <div class='column-3'>
+          <img class='icon' src='../../assets/icon-vc-backing.svg'>
+          <h3 class='feature-title'>
+            Market Leader
+          </h3>
+          <p>Our partners include top tier global VCs as well as the world's largest PoC mining pool. Our years of experience allow us to accurately identify market trends, conduct in-depth due diligence, and target upcoming superstar projects across multiple sectors in the industry, bringing them to our community of key opinion leaders and investors.</p>
+        </div>
+
+        <div class='column-3'>
+          <img class='icon' src='../../assets/icon-team.svg'>
+          <h3 class='feature-title'>
+            Dream Team
+          </h3>
+          <p>Procyon’s founders have over 10 years of cryptocurrency market experience with a track record that includes investment projects with average ROI of over 100x. Our tech team has over 5 years of experience managing assets totalling over 160 million USD. Our Japan-based team provides exceptional, localized, and reliable customer service within 24 hours.</p>
+        </div>
+
+        <div class='column-3'>
+          <img class='icon' src='../../assets/icon-technology.svg'>
+          <h3 class='feature-title'>
+            The Big Picture
+          </h3>
+          <p>The market flows from R&amp;D to VC funds to Miners to Exchanges and is finally distributed to general investors. At Procyon we aim to find opportunities that are usually only available to institutional investors and make them equally accessible to individual investors. Join our exclusive community of wealth generation!</p>
+        </div>
+      </div>
+    </div>
+    <div class='hr' />
+  </div>
+</template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,9 @@
     <Banner />
     <Products />
     <WhyMe />
+  </div>
+  <div class='content2'>
+    <Feature />
     <div class='hr' />
   </div>
 </template>
@@ -13,5 +16,14 @@ import { defineAsyncComponent } from 'vue'
 const Banner = defineAsyncComponent(() => import('src/components/index/Banner.vue'))
 const Products = defineAsyncComponent(() => import('src/components/index/Products.vue'))
 const WhyMe = defineAsyncComponent(() => import('src/components/index/WhyMe.vue'))
+const Feature = defineAsyncComponent(() => import('src/components/index/Feature.vue'))
 
 </script>
+
+<style lang='sass' scoped>
+.content2
+  margin: 0 auto
+  padding: 0 24px
+  @media (max-width: $breakpoint-sm-max)
+  padding: 0 12px
+</style>
