e2227bd2749b24252cc11d3e9b027fc6343857c1
Zhao KK <kikakkz@hotmail.com>
2022-04-09 20:19:57 +0800

    Add mining card

--- src/components/dashboard/MiningCard.vue
+++ src/components/dashboard/MiningCard.vue
@@ -2,55 +2,129 @@
   <div class='mining-summary content-glass'>
     <div class='mining-heading'>
       <div class='product-page-icon'>
-        <img src='product-spacemesh.svg'>
+        <img :src='coin?.Logo'>
       </div>
-      <h3 class='mining-title'>Spacemesh</h3>
+      <h3 class='mining-title'>
+        {{ coin?.Name }}
+      </h3>
     </div>
     <div class='top-line-summary'>
       <div class='top-line-item'>
-        <span class='label'>Earnings:</span>
-        <span class='value'>10 SMH</span>
-        <span class='sub-value'>(5,000 USDT)</span>
+        <span class='label'>{{ $t('MSG_EARNINGS') }}:</span>
+        <span class='value'>{{ coin?.PreSale ? '*' : _totalEarningCoin }} {{ coin?.Unit }}</span>
+        <span class='sub-value'>({{ totalEarningUSD }} USDT)</span>
       </div>
       <div class='top-line-item'>
-        <span class='label'>Last 24 hours:</span>
-        <span class='value'>.5 SMH</span>
-        <span class='sub-value'>(250 USDT)</span>
+        <span class='label'>{{ $t('MSG_LAST_24_HOURS') }}:</span>
+        <span class='value'>{{ coin?.PreSale ? '*' : _last24HoursEarningCoin }} {{ coin.Unit }}</span>
+        <span class='sub-value'>({{ last24HoursEarningUSD }} USDT)</span>
       </div>
       <div class='top-line-item'>
-        <span class='label'>Capacity:</span>
-        <span class='value'>100 TB</span>
+        <span class='label'>{{ $t('MSG_CAPACITY') }}:</span>
+        <span class='value'>{{ totalUnits }} {{ goodUnit }}</span>
       </div>
     </div>
     <div class='detailed-summary'>
       <div class='line'>
-        <span class='label'>Daily Output (30 day avg.):</span>
-        <span class='value'>1 SMH</span>
+        <span class='label'>{{ $t('MSG_30_DAYS_AVERAGE_OUTPUT') }}:</span>
+        <span class='value'>{{ coin?.PreSale ? '*' : _last30DaysEarningCoin / 30 }} {{ coin.Unit }}</span>
       </div>
       <div class='line'>
-        <span class='label'>Technical Service Fee:</span>
-        <span class='value'>.2 SMH (20%)</span>
+        <span class='label'>{{ $t('MSG_TECHNIQUE_SERVICE_FEE') }}:</span>
+        <span class='value'>{{ coin?.PreSale ? '*' : _last24HoursEarningCoin * 0.2 }} {{ coin.Unit }} (20%)</span>
       </div>
       <div class='line'>
-        <span class='label'>Net daily output (30 day avg.):</span>
-        <span class='value'>.8 SMH</span>
+        <span class='label'>{{ $t('MSG_30_DAYS_AVERAGE_NET_OUTPUT') }}:</span>
+        <span class='value'>{{ coin?.PreSale ? '*' : _last30DaysEarningCoin / 30 * 0.8 }} {{ coin.Unit }}</span>
       </div>
       <div class='line'>
-        <span class='label'>Total days mining:</span>
-        <span class='value'>150 days</span>
+        <span class='label'>{{ $t('MSG_SERVICE_PERIOD') }}:</span>
+        <span class='value'>{{ goodPeriod }} {{ $t('MSG_DAYS') }}</span>
       </div>
       <div class='line'>
-        <span class='label'>Hashrate (30 day avg.):</span>
-        <span class='value'>950 MH/s</span>
-      </div>
-      <div class='line'>
-        <span class='label'>Network Daily Ouput:</span>
-        <span class='value'>1,000,000 SMH</span>
+        <span class='label'>{{ $t('MSG_NETWORK_DAILY_OUTPUT') }}:</span>
+        <span class='value'>{{ coin?.PreSale ? '*' : 100000 }} {{ coin.Unit }}</span>
       </div>
     </div>
     <div class='buttons'>
-      <button class='alt'>Export Daily Output (CSV)</button>
-      <button>Purchase Capacity</button>
+      <button class='alt' disabled>
+        {{ $t('MSG_EXPORT_DAILY_OUTPUT_CSV') }}
+      </button>
+      <button @click='onPurchaseClick'>
+        {{ $t('MSG_PURCHASE_CAPACITY') }}
+      </button>
     </div>
   </div>
 </template>
+
+<script setup lang='ts'>
+import {
+  totalEarningCoin,
+  useCoinStore,
+  useCurrencyStore,
+  Currency,
+  last30DaysEarningCoin,
+  last24HoursEarningCoin,
+  useOrderStore
+} from 'npool-cli-v2'
+import { defineProps, toRef, computed, onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+interface Props {
+  coinTypeId: string
+}
+
+const props = defineProps<Props>()
+const coinTypeId = toRef(props, 'coinTypeId')
+
+const coins = useCoinStore()
+const coin = computed(() => coins.getCoinByID(coinTypeId.value))
+
+const order = useOrderStore()
+const orders = computed(() => order.Orders.filter((myOrder) => {
+  return myOrder?.Good?.Main?.ID === coin.value.ID && order.validateOrder(myOrder)
+}))
+const goodUnit = computed(() => order.Orders.length > 0 ? order.Orders[0].Good.Good.Good.Unit : '')
+const goodPeriod = computed(() => order.Orders.length > 0 ? order.Orders[0].Good.Good.Good.DurationDays : '')
+const totalUnits = computed(() => orders.value.reduce((sum, b) => sum + b.Order.Order.Units, 0))
+
+const currency = useCurrencyStore()
+
+const _totalEarningCoin = ref(0)
+const totalEarningUSD = ref(0)
+
+const _last24HoursEarningCoin = ref(0)
+const last24HoursEarningUSD = ref(0)
+
+const _last30DaysEarningCoin = ref(0)
+const last30DaysEarningUSD = ref(0)
+
+onMounted(() => {
+  totalEarningCoin(coinTypeId.value, (coinAmount: number) => {
+    _totalEarningCoin.value = coinAmount
+    currency.getCoinCurrency(coin.value, Currency.USD, (currency: number) => {
+      last30DaysEarningUSD.value = _totalEarningCoin.value * currency
+    })
+  })
+
+  last30DaysEarningCoin(coinTypeId.value, (coinAmount: number) => {
+    _last30DaysEarningCoin.value = coinAmount
+    currency.getCoinCurrency(coin.value, Currency.USD, (currency: number) => {
+      totalEarningUSD.value = _totalEarningCoin.value * currency
+    })
+  })
+
+  last24HoursEarningCoin(coinTypeId.value, (coinAmount: number) => {
+    _last24HoursEarningCoin.value = coinAmount
+    currency.getCoinCurrency(coin.value, Currency.USD, (currency: number) => {
+      last24HoursEarningUSD.value = _last24HoursEarningCoin.value * currency
+    })
+  })
+})
+
+const router = useRouter()
+const onPurchaseClick = () => {
+  void router.push({ path: '/' })
+}
+
+</script>
--- src/components/dashboard/MiningCards.vue
+++ src/components/dashboard/MiningCards.vue
@@ -1,11 +1,58 @@
 <template>
   <h2>{{ $t('MSG_MINING_DASHBOARD') }}</h2>
-  <MiningCard />
+  <MiningCard v-for='myCoin in coins' :key='myCoin.ID' :coin-type-id='(myCoin.ID as string)' />
 </template>
 
 <script setup lang='ts'>
-import { defineAsyncComponent } from 'vue'
+import { useCoinStore, useOrderStore, NotificationType } from 'npool-cli-v2'
+import { defineAsyncComponent, computed, onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+// eslint-disable-next-line @typescript-eslint/unbound-method
+const { t } = useI18n({ useScope: 'global' })
 
 const MiningCard = defineAsyncComponent(() => import('src/components/dashboard/MiningCard.vue'))
+
+const coin = useCoinStore()
+const orders = useOrderStore()
+
+const coins = computed(() => coin.Coins.filter((coin) => {
+  for (const order of orders.Orders) {
+    if (coin.ID === order.Good.Main?.ID) {
+      return true
+    }
+  }
+  return false
+}))
+
+onMounted(() => {
+  if (coin.Coins.length === 0) {
+    coin.getCoins({
+      Message: {
+        Error: {
+          Title: t('MSG_GET_COINS'),
+          Message: t('MSG_GET_COINS_FAIL'),
+          Popup: true,
+          Type: NotificationType.Error
+        }
+      }
+    }, () => {
+      // TODO
+    })
+  }
+
+  if (orders.Orders.length === 0) {
+    orders.getOrders({
+      Message: {
+        Error: {
+          Title: t('MSG_GET_ORDERS'),
+          Message: t('MSG_GET_ORDERS_FAIL'),
+          Popup: true,
+          Type: NotificationType.Error
+        }
+      }
+    })
+  }
+})
 
 </script>
