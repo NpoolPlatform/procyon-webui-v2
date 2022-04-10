614d53d6a7d1c75a8d38b5bc4fd880a7450657fd
Zhao KK <kikakkz@hotmail.com>
2022-04-10 12:30:23 +0800

    Add jsx

--- src/components/affiliates/Tree.vue
+++ src/components/affiliates/Tree.vue
@@ -1,123 +1,77 @@
 <template>
-  <ShowSwitchTable
-    label='MSG_APPROVED_ADDRESSES'
-    :rows='(accounts as Array<never>)'
-    :table='(table as never)'
-    :customize-body='true'
-  >
-    <template #top-right>
-      <div class='buttons'>
-        <button @click='onAddNewAddressClick'>
-          {{ $t('MSG_ADD_NEW_ADDRESS') }}
-        </button>
+  <h2>{{ $t('MSG_AFFILIATE_TREE') }}</h2>
+  <q-tree
+    :nodes='referralTree'
+    node-key='UserID'
+    default-expand-all :expanded='[logined.LoginedUser?.User.ID]'>
+    <template #default-header='prop'>
+      <div class='invitation-box'>
+        <div class='invitation-header'>
+          <div class='header-content row'>
+            <span style='font-size: 20px; font-weight: 600;'>
+              {{ prop.node.Referral.Extra ? prop.node.Referral.Extra.Username : prop.node.Referral.User?.EmailAddress }}
+            </span>
+            <q-space />
+            <span
+              style='font-size: 14px; font-weight: 300; margin-right: 5px; margin-top: 6px;'
+            >
+              {{ $t('affiliate.OnBoarded') }}
+            </span>
+            <span class='invited-count'>{{ prop.node.InvitedCount }}</span>
+          </div>
+        </div>
+
+        <div class='invitation-content'>
+          <span>{{ prop.node.Referral.User?.EmailAddress }}</span>
+        </div>
+
+        <div class='invitation-content'>
+          <div v-for='summary in prop.node.Referral.Summaries' :key='summary.CoinTypeID'>
+            <span>{{ summary.CoinName }}: </span>
+            <span class='sales-number'>{{ summary.Units }}</span>
+            <span> {{ summary.Unit }} / </span>
+            <span class='sales-number'>{{ Math.floor(summary.Amount) }}</span>
+            <span> USDT</span>
+          </div>
+        </div>
       </div>
+      <q-inner-loading dark :showing='innerLoading' v-if='userInfo.User.ID === prop.node.UserID'>
+        <q-spinner-gears size='50px' color='primary' />
+      </q-inner-loading>
     </template>
-    <template #table-body='myProps'>
-      <q-tr :props='myProps'>
-        <q-td key='Blockchain' :props='myProps'>
-          <LogoName
-            :logo='coin.getCoinByID(myProps.row.Address.CoinTypeID)?.Logo'
-            :name='coin.getCoinByID(myProps.row.Address.CoinTypeID)?.Name'
-          />
-        </q-td>
-        <q-td key='Address' :props='myProps'>
-          {{ myProps.row.Account.Address }}
-        </q-td>
-        <q-td key='Label' :props='myProps'>
-          {{ myProps.row.Address.Labels?.join(',') }}
-        </q-td>
-        <q-td key='DateAdded' :props='myProps'>
-          {{ formatTime(myProps.row.Address.CreateAt) }}
-        </q-td>
-        <q-td key='ActionButtons' :props='myProps'>
-          <button class='small'>
-            {{ $t('MSG_REMOVE') }}
-          </button>
-        </q-td>
-      </q-tr>
-    </template>
-  </ShowSwitchTable>
+  </q-tree>
 </template>
 
 <script setup lang='ts'>
-import { computed, onMounted, defineAsyncComponent } from 'vue'
-import { NotificationType, useCoinStore, WithdrawAccount, formatTime, useAccountStore } from 'npool-cli-v2'
+import { computed, onMounted, ref } from 'vue'
+import { NotificationType, useInspireStore, buildReferralTree, useLoginedUserStore } from 'npool-cli-v2'
 import { useI18n } from 'vue-i18n'
-
-const ShowSwitchTable = defineAsyncComponent(() => import('src/components/table/ShowSwitchTable.vue'))
-const LogoName = defineAsyncComponent(() => import('src/components/logo/LogoName.vue'))
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
 
-const coin = useCoinStore()
-const account = useAccountStore()
-const accounts = computed(() => account.Accounts)
+const inspire = useInspireStore()
+const referralTree = computed(() => buildReferralTree(inspire.Referrals))
 
-const table = computed(() => [
-  {
-    name: 'Blockchain',
-    label: t('MSG_BLOCKCHAIN'),
-    align: 'left',
-    field: (row: WithdrawAccount) => coin.getCoinByID(row.Address.CoinTypeID)?.Name
-  },
-  {
-    name: 'Address',
-    label: t('MSG_ADDRESS'),
-    align: 'center',
-    field: (row: WithdrawAccount) => row.Account.Address
-  },
-  {
-    name: 'Label',
-    label: t('MSG_LABEL'),
-    align: 'center',
-    field: (row: WithdrawAccount) => row.Address.Labels?.join(',')
-  },
-  {
-    name: 'DateAdded',
-    label: t('MSG_DATE_ADDED'),
-    align: 'center',
-    field: (row: WithdrawAccount) => formatTime(row.Address.CreateAt)
-  },
-  {
-    name: 'ActionButtons',
-    label: '',
-    align: 'center',
-    field: ''
-  }
-])
+const logined = useLoginedUserStore()
+const innerLoading = ref(false)
 
 onMounted(() => {
-  if (coin.Coins.length === 0) {
-    coin.getCoins({
+  if (inspire.Referrals.length === 0) {
+    innerLoading.value = true
+    inspire.getReferrals({
       Message: {
         Error: {
-          Title: t('MSG_GET_COINS_FAIL'),
+          Title: t('MSG_GET_REFERRALS_FAIL'),
           Popup: true,
           Type: NotificationType.Error
         }
       }
     }, () => {
-      // TODO
-    })
-  }
-
-  if (accounts.value.length === 0) {
-    account.getWithdrawAccounts({
-      Message: {
-        Error: {
-          Title: t('MSG_GET_WITHDRAW_ACCOUNTS_FAIL'),
-          Popup: true,
-          Type: NotificationType.Error
-        }
-      }
+      innerLoading.value = false
     })
   }
 })
-
-const onAddNewAddressClick = () => {
-  // TODO
-}
 
 </script>
 
