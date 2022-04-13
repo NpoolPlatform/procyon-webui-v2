30a16e8f007d45ee4d43ae2529f88213197e6247
Zhao KK <kikakkz@hotmail.com>
2022-04-13 16:16:21 +0800

    Of language is set, use set one

--- src/components/lang/LangSwitcher.vue
+++ src/components/lang/LangSwitcher.vue
@@ -2,12 +2,12 @@
   <ul class='language-picker'>
     <li
       class='selected'
-      v-for='language in lang.Languages'
+      v-for='language in locale.Languages'
       :key='language.ID'
       @click='onLangClick(language)'
     >
       <a class='language'>
-        {{ language.Short.length > 0 ? language.Short : language.Lang }}
+        {{ language.Short?.length > 0 ? language.Short : language.Lang }}
       </a>
     </li>
   </ul>
@@ -15,10 +15,13 @@
 
 <script setup lang='ts'>
 
-import { useLangStore, Language } from 'npool-cli-v2'
+import { Language, useLangStore, useLocaleStore } from 'npool-cli-v2'
 
+const locale = useLocaleStore()
 const lang = useLangStore()
+
 const onLangClick = (language: Language) => {
+  console.log(language)
   lang.setLang(language)
 }
 
