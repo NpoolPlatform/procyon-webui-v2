6b3544e6b86d8faa67e346e161e63342afd185a3
Zhao KK <kikakkz@hotmail.com>
2022-04-25 19:28:03 +0800

    Use default email account type

--- src/components/user/SignPage.vue
+++ src/components/user/SignPage.vue
@@ -120,7 +120,7 @@ const emit = defineEmits<{(e: 'update:accountType', type: string): void,
   (e: 'submit'): void
 }>()
 
-const signupMethod = ref(AccountType.Mobile)
+const signupMethod = ref(AccountType.Email)
 const onSwitcherClick = () => {
   switch (signupMethod.value) {
     case AccountType.Email:
