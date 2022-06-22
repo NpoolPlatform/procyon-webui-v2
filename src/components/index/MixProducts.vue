0708d40d4015c927696a5f9207f5efc6841421f8
Zhao KK <kikakkz@hotmail.com>
2022-06-22 15:04:40 +0800

    Add mix products

--- src/components/index/MixProducts.vue
+++ src/components/index/MixProducts.vue
@@ -1,26 +1,43 @@
 <template>
   <div class='content'>
-    <h2>{{ $t('MSG_PREMIERE_PRODUCTS') }}</h2>
-    <AleoCardLarge
-      v-show='firstGood'
-      :good='firstGood'
-      project-class='project-aleo'
-      bg-img='product/aleo/aleo-banner.jpg'
-      purchase-caption='MSG_ALEO_PURCHASE_CAPTION'
-    />
+    <div class='columns-2 highlight'>
+      <div class='column-2 order-1 with-frame'>
+        <h1>First-batch mining</h1>
+        <h2>The best mining rewards come to those who start first</h2>
+        <ul>
+          <li>First-to-market mining products</li>
+          <li>Globally proven success record</li>
+          <li>Expertly curated product selection</li>
+        </ul>
+        <button>Learn more</button>
+      </div>
+      <div class='column-2 order-2'>
+        <img src='product/aleo/procyon-product-aleo-homepage.png'>
+      </div>
+      <div class='column-2 order-4'>
+        <img src='product/mixmob/procyon-ido-mixmob-homepage.png'>
+      </div>
+      <div class='column-2 order-3 with-frame'>
+        <h1>Exclusive IDO</h1>
+        <h2>VC-level opportunities for everyone</h2>
+        <ul>
+          <li>Top-tier projects backed by leading VCs</li>
+          <li>Highly-vetted by rigorous due diligence</li>
+          <li>Early access, limited supply</li>
+        </ul>
+        <button class='in-active'>Coming soon</button>
+      </div>
+    </div>
   </div>
-  <div class='hr' />
 </template>
 
 <script setup lang='ts'>
-import { computed, defineAsyncComponent, onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useGoodStore, NotificationType } from 'npool-cli-v2'
 import { useI18n } from 'vue-i18n'
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
-
-const AleoCardLarge = defineAsyncComponent(() => import('src/components/product/AleoCardLarge.vue'))
 
 const good = useGoodStore()
 const firstGood = computed(() => good.getRecommendGoods?.[0])
@@ -74,3 +91,14 @@ onMounted(() => {
 })
 
 </script>
+
+<style lang='sass' scoped>
+.order-4,
+.order-2
+  min-width: 49%
+
+.order-4
+  margin-left: -40px
+  @media (max-width: $breakpoint-sm-max)
+    margin-left: 0
+</style>
