eb5b704c813440f4069ea62ff8f9bdc0bce48674
Zhao KK <kikakkz@hotmail.com>
2022-08-09 16:09:34 +0800

    Add profit code

--- src/components/dashboard/MiningSummary.vue
+++ src/components/dashboard/MiningSummary.vue
@@ -36,18 +36,13 @@
 import {
   Currency,
   useCurrencyStore,
-  NotificationType,
   useCoinStore,
-  PriceCoinName,
-  SecondsEachDay
+  PriceCoinName
 } from 'npool-cli-v2'
+import { IntervalKey } from 'src/const/const'
 import { useProfitStore } from 'src/teststore/mock/profit'
 // import { onMounted, ref, computed } from 'vue'
-import { computed, onMounted, ref } from 'vue'
-import { useI18n } from 'vue-i18n'
-
-// eslint-disable-next-line @typescript-eslint/unbound-method
-const { t } = useI18n({ useScope: 'global' })
+import { computed, ref } from 'vue'
 
 const totalEarning = ref(0)
 const totalWithdrawed = ref(0)
@@ -71,52 +66,6 @@ const totalProfit = computed(() => {
   return total
 })
 
-enum IntervalKey {
-  All = 'All',
-  LastDay = 'LastDay',
-  LastMonth = 'LastMonty'
-}
-
-const getIntervalProfits = (key: IntervalKey, startAt: number, endAt: number, offset:number, limit: number) => {
-  profit.getIntervalProfits({
-    StartAt: startAt,
-    EndAt: endAt,
-    Offset: offset,
-    Limit: limit,
-    Message: {
-      Error: {
-        Title: t('MSG_GET_INTERVAL_PROFIT_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, key, () => {
-    if (profit.CoinProfits.get(key)?.Profits?.length === profit.CoinProfits.get(key)?.Total) {
-      return
-    }
-    getIntervalProfits(key, startAt, endAt, limit + offset, limit)
-  })
-}
-
-const getProfits = (offset:number, limit: number) => {
-  profit.getProfits({
-    Offset: offset,
-    Limit: limit,
-    Message: {
-      Error: {
-        Title: t('MSG_GET_INTERVAL_PROFIT_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    if (profit.Profits.Total === profit.Profits.Profits.length) {
-      return
-    }
-    getProfits(limit + offset, limit)
-  })
-}
-
 const last24HoursEarning = computed(() => {
   let total = 0
   profit.GoodProfits.get(IntervalKey.LastDay)?.Profits?.forEach((el) => {
@@ -129,17 +78,6 @@ const last24HoursEarning = computed(() => {
     )
   })
   return total
-})
-
-onMounted(() => {
-  if (profit.CoinProfits.size === 0) {
-    getProfits(0, 100)
-    getIntervalProfits(
-      IntervalKey.LastDay,
-      Math.ceil(new Date().getTime() / 1000) - SecondsEachDay,
-      Math.ceil(new Date().getTime() / 1000),
-      0, 100)
-  }
 })
 
 </script>
