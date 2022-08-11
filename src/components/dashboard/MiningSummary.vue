e98ef7ffcbc6364b950771a2c8da6e1b2549b546
Zhao KK <kikakkz@hotmail.com>
2022-08-11 16:22:05 +0800

    Add profit amount

--- src/components/dashboard/MiningSummary.vue
+++ src/components/dashboard/MiningSummary.vue
@@ -33,36 +33,13 @@
 </template>
 
 <script setup lang="ts">
-import {
-  Currency,
-  useCurrencyStore,
-  useCoinStore,
-  PriceCoinName
-} from 'npool-cli-v2'
-import { IntervalKey } from 'src/const/const'
+import { PriceCoinName } from 'npool-cli-v2'
 import { useLocalLedgerStore } from 'src/localstore/ledger'
-import { useProfitStore } from 'src/teststore/mock/profit'
 import { computed } from 'vue'
-
-const currency = useCurrencyStore()
-const coin = useCoinStore()
-const profit = useProfitStore()
 
 const localledger = useLocalLedgerStore()
 
 const totalProfit = computed(() => localledger.Profit.USDAmount)
-const last24HoursEarning = computed(() => {
-  let total = 0
-  profit.GoodProfits.get(IntervalKey.LastDay)?.Profits?.forEach((el) => {
-    currency.getCoinCurrency(
-      coin.getCoinByID(el.CoinTypeID),
-      Currency.USD,
-      (currency) => {
-        total += currency * Number(el.Incoming)
-      }
-    )
-  })
-  return total
-})
+const last24HoursEarning = computed(() => localledger.Profit.Last24HourUSDAmount)
 
 </script>
