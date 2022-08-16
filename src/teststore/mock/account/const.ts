12d28bd9f157b19be6963caee33bc96523b43ebd
daqiang.li <dqli161@163.com>
2022-08-16 21:00:34 +0800

    Add account api

---
+++ src/teststore/mock/account/const.ts
@@ -0,0 +1,5 @@
+enum API {
+  GET_USER_ACCOUNTS = '/account/v1/get/useraccounts',
+  CREATE_ACCOUNT = '/account/v1/create/account'
+}
+export { API }
---
+++ src/teststore/mock/account/types.ts
@@ -0,0 +1,43 @@
+import { BaseRequest } from 'npool-cli-v2/.'
+import { AccountUsedFor } from './state'
+
+interface Account {
+  ID: string;
+  AppID: string;
+  UserID: string;
+  CoinTypeID: string;
+  CoinName: string;
+  CoinUnit: string;
+  CoinEnv: string;
+  CoinLogo: string;
+  AccountID: string;
+  Address: string;
+  UsedFor: string;
+  Labels: string[];
+  CreatedAt: number;
+}
+interface CreateAccountRequest extends BaseRequest {
+  CoinTypeID: string;
+  UsedFor: AccountUsedFor;
+  Address?: string;
+  Labels?: string[];
+}
+interface CreateAccountResponse {
+  Info: Account;
+}
+interface GetUserAccountsRequest extends BaseRequest{
+  UsedFor: AccountUsedFor;
+  Offset?: number;
+  Limit?: number;
+}
+interface GetUserAccountsResponse {
+  Infos: Array<Account>;
+  Total: number;
+}
+export {
+  Account,
+  CreateAccountRequest,
+  CreateAccountResponse,
+  GetUserAccountsRequest,
+  GetUserAccountsResponse
+}
