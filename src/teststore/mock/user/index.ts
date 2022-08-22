c84399a8db5db984cf7d9b60273f418fdd37e1a1
daqiang.li <dqli161@163.com>
2022-08-22 19:52:11 +0800

    Login && Logout

--- src/teststore/mock/user/index.ts
+++ src/teststore/mock/user/index.ts
@@ -1,15 +1,30 @@
 import { doAction } from 'npool-cli-v2'
 import { defineStore } from 'pinia'
 import { API } from './const'
-import { SignupRequest, SignupResponse, UserState, LoginHistory } from './types'
+import { Cookies } from 'quasar'
+import {
+  SignupRequest,
+  SignupResponse,
+  UserState,
+  LoginHistory,
+  LoginRequest,
+  User,
+  LoginVerifyRequest,
+  LoginResponse,
+  LoginVerifyResponse,
+  LogoutRequest,
+  LogoutResponse
+} from './types'
 
-export const useUserStore = defineStore('luser', {
+export const useUserStore = defineStore('localuser', {
   state: (): UserState => ({
     LoginHistories: [] as Array<LoginHistory>,
     GoogleOTPAuth: '',
-    GoogleSecret: ''
+    GoogleSecret: '',
+    LoginedUser: {} as User
   }),
   getters: {
+    getLogined (): boolean { return this.LoginedUser !== undefined && this.LoginedUser !== null }
   },
   actions: {
     signup (req: SignupRequest, done: () => void) {
@@ -19,6 +34,44 @@ export const useUserStore = defineStore('luser', {
         req.Message,
         (): void => {
           done()
+        })
+    },
+    login (req: LoginRequest, done: () => void) {
+      doAction<LoginRequest, LoginResponse>(
+        API.LOGIN,
+        req,
+        req.Message,
+        (resp: LoginResponse): void => {
+          Cookies.set('X-User-ID', resp.Info.ID, { expires: '4h', secure: true })
+          Cookies.set('X-App-Login-Token', resp.Info.LoginToken, { expires: '4h', secure: true })
+          done()
+        })
+    },
+    loginVerify (req: LoginVerifyRequest, done: () => void) {
+      doAction<LoginVerifyRequest, LoginVerifyResponse>(
+        API.LOGIN_VERIFY,
+        req,
+        req.Message,
+        (resp: LoginVerifyResponse): void => {
+          if (resp.Info?.LoginVerified) {
+            this.LoginedUser = resp.Info
+            Cookies.set('X-User-ID', resp?.Info.ID, { expires: '4h', secure: true })
+            Cookies.set('X-App-Login-Token', resp.Info.LoginToken, { expires: '4h', secure: true })
+          }
+          done()
+        })
+    },
+    logout (req: LogoutRequest, done?: () => void) {
+      doAction<LogoutRequest, LogoutResponse>(
+        API.LOGOUT,
+        req,
+        req.Message,
+        (resp: LogoutResponse): void => {
+          Cookies.remove('X-User-ID')
+          Cookies.remove('X-AppLogin-Token')
+          console.log('logout: ', resp)
+          this.LoginedUser = undefined as unknown as User
+          done?.()
         })
     }
   }
