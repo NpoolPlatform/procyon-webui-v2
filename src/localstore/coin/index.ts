92a79712fb05b2275065aae1bdb0098a4f5e60db
daqiang.li <dqli161@163.com>
2022-08-23 11:28:13 +0800

    Format coin name

---
+++ src/localstore/coin/index.ts
@@ -0,0 +1,23 @@
+import { useCurrencyStore } from 'npool-cli-v2'
+import { defineStore } from 'pinia'
+export const useLocalCoinStore = defineStore('localcoin', {
+  state: () => ({}),
+  getters: {
+    formatCoinName: () => (coinName:string) => {
+      const currencies = useCurrencyStore()
+      if (coinName?.toLowerCase()?.includes('bitcoin')) {
+        return 'BTC (Bitcoin)'
+      } else if (coinName?.toLowerCase()?.includes('binanceusd')) {
+        return 'BUSD (BEP20)'
+      } else if (coinName?.toLowerCase()?.includes('usdcerc20')) {
+        return 'USDC (ERC20)'
+      } else if (coinName?.toLowerCase()?.includes('usdterc20')) {
+        return 'USDT (ERC20)'
+      } else if (coinName?.toLowerCase()?.includes('usdttrc20')) {
+        return 'USDT (TRC20)'
+      }
+      return currencies.formatCoinName(coinName)
+    }
+  },
+  actions: {}
+})
