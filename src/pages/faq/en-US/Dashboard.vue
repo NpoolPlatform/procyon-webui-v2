8d2f490508887b273d9162de88ebdf705c399246
lidaqiang <dqli161@163.com>
2022-09-21 16:48:36 +0800

    Update FAQ Dashboard Page

--- src/pages/faq/en-US/Dashboard.vue
+++ src/pages/faq/en-US/Dashboard.vue
@@ -9,12 +9,8 @@
 
           <ul class='faq-nav-links'>
             <li><a @click='scrollTo("How do I check my mining rewards?")'>How do I check my mining rewards?</a></li>
-            <li><a @click='scrollTo("What is SMH?")'>What is SMH?</a></li>
-            <li>
-              <a @click='scrollTo("Why does the amount of SMH rewards change from day to day?")'>
-                Why does the amount of SMH rewards change from day to day?
-              </a>
-            </li>
+            <li><a @click='scrollTo("Which products can I see on my Dashboard?")'>Which products can I see on my Dashboard?</a></li>
+            <li><a @click='scrollTo("Why does the amount of rewards change from day to day?")'>Why does the amount of rewards change from day to day?</a></li>
             <li><a @click='scrollTo("How can I check my order history?")'>How can I check my order history?</a></li>
             <li><a @click='scrollTo("How to export mining data (CSV data)?")'>How to export mining data (CSV data)?</a></li>
             <li><a @click='scrollTo("Why dont numbers change on the dashboard?")'>Why don't numbers change on the dashboard?</a></li>
@@ -37,23 +33,17 @@
             <li>(2) <strong>[Total Earnings]</strong> and <strong>[Mining Dashboard]</strong> show your mining rewards.</li>
           </ul>
 
-          <h3 id='What is SMH?'>
-            What is SMH?
+          <h3 id='Which products can I see on my Dashboard?'>
+            Which products can I see on my Dashboard?
           </h3>
 
-          <p>
-            SMH is Spacemesh's native token. When you purchase our SMH mining product, it will begin to mine SMH and
-            the daily rewards will be airdropped into your <strong>[Dashboard]</strong> on Procyon.
-          </p>
+          <p>Only mining products you have purchase will appear on your <strong>[Dashboard]</strong>. When you purchase a mining product, it will begin to mine tokens and the daily rewards will be airdropped into your <strong>[Dashboard]</strong> on Procyon.</p>
 
-          <h3 id='Why does the amount of SMH rewards change from day to day?'>
-            Why does the amount of SMH rewards change from day to day?
+          <h3 id='Why does the amount of rewards change from day to day?'>
+            Why does the amount of rewards change from day to day?
           </h3>
 
-          <p>
-            The amount of mining rewards varies depending on the mining performance. Differences in performance are
-            influenced by market conditions (e.g. the number of miners, mining difficulties etc.).
-          </p>
+          <p>The amount of mining rewards varies depending on the mining performance. Differences in performance are influenced by market conditions (e.g. the number of miners, mining difficulties etc.).</p>
 
           <h3 id='How can I check my order history?'>
             How can I check my order history?
@@ -70,56 +60,39 @@
 
           <ul>
             <li>(1) Open the <strong>[Dashboard]</strong> page after logging in.</li>
-            <li>(2) At the bottom of <strong>[Mining Dashboard]</strong>, click <strong>[Export Daily Output (CSV)]</strong>.</li>
+            <li>(2) At the bottom of <strong>[Mining Dashboard]</strong>, click [Export Daily Output (CSV)].</li>
           </ul>
 
           <h3 id='Why dont numbers change on the dashboard?'>
             Why don't numbers change on the dashboard?
           </h3>
 
-          <p>
-            The mining rewards have not been distributed yet because Spacemesh mining has not started. Procyon will update customers about the progress of Spacemesh as soon as information is available.
-          </p>
+          <p>The mining rewards will bring to appear once mining starts for your purchased products. Since Procyon offers first-batch mining, this means you can purchase mining before it can begin. In such cases you have to wait until the mainnet launches and mining can begin.</p>
 
           <h3 id='What is the Technical Service Fee?'>
             What is the Technical Service Fee?
           </h3>
 
-          <p>
-            The Technical Service Fee covers the maintenance and operation costs of your mining machine. It is
-            automatically deducted from your daily mining rewards.
-          </p>
+          <p>The Technical Service Fee covers the maintenance and operation costs of your mining machine. It is automatically deducted from your daily mining rewards.</p>
 
-          <h3 id='What is hashrate?？'>
+          <h3 id='What is hashrate?'>
             What is hashrate?
           </h3>
 
-          <p>
-            Hashrate is a number that represents the mining speed. The higher the number, the better the mining output.
-          </p>
+          <p>Hashrate is a number that represents the mining speed. For example, it can be calculated in <strong>[MH/s]</strong>, but the units will vary from token to token. The higher the number, the better the mining output.</p>
 
           <h3 id='What is Network Daily Output?'>
             What is Network Daily Output?
           </h3>
 
-          <p>
-            The Network Daily Output shows the overall mining volume on the Spacemesh network. This is determined by
-            the Spacemesh team.
-          </p>
+          <p>The Network Daily Output shows the overall mining volume on the token's network. This is determined by the network itself.</p>
 
           <h3 id='If Procyons servers go down will the mining stop?'>
             If Procyon's servers go down will the mining stop?
           </h3>
 
-          <p>
-            No, mining does not stop even if Procyon's server goes down. The mining is processed at the following data
-            centers in United States of America and each center is operating independently, so the mining would continue without
-            interruption. The mining itself will not stop however the display of your mining rewards may be down until
-            the server recovers.
-          </p>
-          <p>
-            <strong>Data center location: </strong>Dallas, Texas, United States of America
-          </p>
+          <p>No, mining does not stop even if Procyon's server goes down. The mining is processed at the following data centers in Hong Kong and each center is operating independently, so the mining would continue without interruption. The mining itself will not stop however the display of your mining rewards may be down until the server recovers.</p>
+          <p><strong>Data center location: </strong>18 Chun Yat Street, Tseung Kwan O Industrial Estate, New Territories, Hong Kong</p>
 
           <div class='hr' />
         </div>
