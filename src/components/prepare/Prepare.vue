1c65c42ff3212ca13fde71af911b41bc805cde0d
lidaqiang <dqli161@163.com>
2022-10-28 15:20:28 +0800

    Update Good In Prepare Page

--- src/components/prepare/Prepare.vue
+++ src/components/prepare/Prepare.vue
@@ -1,14 +1,18 @@
 <script setup lang='ts'>
-import { useCoinStore, useGoodStore, NotificationType, useCurrencyStore, Currency } from 'npool-cli-v2'
+import { useCoinStore, NotificationType, useCurrencyStore, Currency } from 'npool-cli-v2'
 import { AppID } from 'src/const/const'
 import { onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { NotifyType, useFrontendAppStore } from 'npool-cli-v4'
+import { AppGood, NotifyType, Promotion, Recommend, useAdminAppGoodStore, useAdminPromotionStore, useAdminRecommendStore, useFrontendAppStore } from 'npool-cli-v4'
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
 
 const coin = useCoinStore()
-const good = useGoodStore()
+
+const good = useAdminAppGoodStore()
+const recommend = useAdminRecommendStore()
+const promotion = useAdminPromotionStore()
+
 const currency = useCurrencyStore()
 const application = useFrontendAppStore()
 
@@ -44,38 +48,15 @@ const getCoins = () => {
 }
 
 const getGoods = () => {
-  good.getGoods({
-    Message: {
-      Error: {
-        Title: t('MSG_GET_GOODS'),
-        Message: t('MSG_GET_GOODS_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    // TODO
-  })
-
-  good.getPromotions({
-    Message: {
-      Error: {
-        Title: t('MSG_GET_PROMOTIONS_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  })
-
-  good.getAppGoods({
-    Message: {
-      Error: {
-        Title: t('MSG_GET_APP_GOODS_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  })
+  if (good.AppGoods.AppGoods.length === 0) {
+    getAppGoods(0, 500)
+  }
+  if (promotion.Promotions.Promotions.length === 0) {
+    getPromotions(0, 500)
+  }
+  if (recommend.Recommends.Recommends.length === 0) {
+    getRecommends(0, 500)
+  }
 }
 
 const getApplication = () => {
@@ -99,4 +80,60 @@ onMounted(() => {
   getApplication()
 })
 
+const getAppGoods = (offset: number, limit: number) => {
+  good.getAppGoods({
+    Offset: offset,
+    Limit: limit,
+    Message: {
+      Error: {
+        Title: t('MSG_GET_APP_GOODS_FAIL'),
+        Popup: true,
+        Type: NotifyType.Error
+      }
+    }
+  }, (g: Array<AppGood>, error: boolean) => {
+    if (error || g.length < limit) {
+      return
+    }
+    getAppGoods(offset + limit, limit)
+  })
+}
+
+const getRecommends = (offset: number, limit: number) => {
+  recommend.getRecommends({
+    Offset: offset,
+    Limit: limit,
+    Message: {
+      Error: {
+        Title: t('MSG_GET_RECOMMENDS_FAIL'),
+        Popup: true,
+        Type: NotifyType.Error
+      }
+    }
+  }, (g: Array<Recommend>, error: boolean) => {
+    if (error || g.length < limit) {
+      return
+    }
+    getRecommends(offset + limit, limit)
+  })
+}
+
+const getPromotions = (offset: number, limit: number) => {
+  promotion.getPromotions({
+    Offset: offset,
+    Limit: limit,
+    Message: {
+      Error: {
+        Title: t('MSG_GET_PROMOTIONS_FAIL'),
+        Popup: true,
+        Type: NotifyType.Error
+      }
+    }
+  }, (g: Array<Promotion>, error: boolean) => {
+    if (error || g.length < limit) {
+      return
+    }
+    getPromotions(offset + limit, limit)
+  })
+}
 </script>
