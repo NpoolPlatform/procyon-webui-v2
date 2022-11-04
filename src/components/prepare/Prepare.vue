8487551a66b9655551ab55199f6d2a583ad220ed
lidaqiang <dqli161@163.com>
2022-11-04 16:23:44 +0800

    Delete Recommend & Promotion

--- src/components/prepare/Prepare.vue
+++ src/components/prepare/Prepare.vue
@@ -3,15 +3,13 @@ import { useCoinStore, NotificationType, useCurrencyStore, Currency } from 'npoo
 import { AppID } from 'src/const/const'
 import { onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { AppGood, NotifyType, Promotion, Recommend, useAdminAppGoodStore, useAdminPromotionStore, useAdminRecommendStore, useFrontendAppStore } from 'npool-cli-v4'
+import { AppGood, NotifyType, useAdminAppGoodStore, useFrontendAppStore } from 'npool-cli-v4'
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
 
 const coin = useCoinStore()
 
 const good = useAdminAppGoodStore()
-const recommend = useAdminRecommendStore()
-const promotion = useAdminPromotionStore()
 
 const currency = useCurrencyStore()
 const application = useFrontendAppStore()
@@ -47,18 +45,6 @@ const getCoins = () => {
   })
 }
 
-const getGoods = () => {
-  if (good.AppGoods.AppGoods.length === 0) {
-    getAppGoods(0, 500)
-  }
-  if (promotion.Promotions.Promotions.length === 0) {
-    getPromotions(0, 500)
-  }
-  if (recommend.Recommends.Recommends.length === 0) {
-    getRecommends(0, 500)
-  }
-}
-
 const getApplication = () => {
   application.getApp({
     AppID: AppID,
@@ -76,7 +62,9 @@ const getApplication = () => {
 
 onMounted(() => {
   getCoins()
-  getGoods()
+  if (good.AppGoods.AppGoods.length === 0) {
+    getAppGoods(0, 500)
+  }
   getApplication()
 })
 
@@ -99,41 +87,4 @@ const getAppGoods = (offset: number, limit: number) => {
   })
 }
 
-const getRecommends = (offset: number, limit: number) => {
-  recommend.getRecommends({
-    Offset: offset,
-    Limit: limit,
-    Message: {
-      Error: {
-        Title: t('MSG_GET_RECOMMENDS_FAIL'),
-        Popup: true,
-        Type: NotifyType.Error
-      }
-    }
-  }, (g: Array<Recommend>, error: boolean) => {
-    if (error || g.length < limit) {
-      return
-    }
-    getRecommends(offset + limit, limit)
-  })
-}
-
-const getPromotions = (offset: number, limit: number) => {
-  promotion.getPromotions({
-    Offset: offset,
-    Limit: limit,
-    Message: {
-      Error: {
-        Title: t('MSG_GET_PROMOTIONS_FAIL'),
-        Popup: true,
-        Type: NotifyType.Error
-      }
-    }
-  }, (g: Array<Promotion>, error: boolean) => {
-    if (error || g.length < limit) {
-      return
-    }
-    getPromotions(offset + limit, limit)
-  })
-}
 </script>
