3ecc9924666fe7cfc8c848f31c613a115b611601
lidaqiang <dqli161@163.com>
2022-11-29 15:03:29 +0800

    update coin prepare page & comment spacemeshmockcard

--- src/components/prepare/Prepare.vue
+++ src/components/prepare/Prepare.vue
@@ -1,67 +1,22 @@
 <script setup lang='ts'>
-import { useCoinStore, NotificationType, useCurrencyStore, Currency } from 'npool-cli-v2'
 import { AppID } from 'src/const/const'
 import { onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { AppGood, NotifyType, useAdminAppGoodStore, useFrontendAppStore } from 'npool-cli-v4'
+import { AppGood, NotifyType, useAdminAppCoinStore, useAdminAppGoodStore, useFrontendAppStore } from 'npool-cli-v4'
+import { getCoins } from 'src/api/coin'
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
 
-const coin = useCoinStore()
+const coin = useAdminAppCoinStore()
 
 const good = useAdminAppGoodStore()
 
-const currency = useCurrencyStore()
 const application = useFrontendAppStore()
 
-const getCurrencies = () => {
-  currency.getAllCoinCurrencies({
-    Currencies: [Currency.USD],
-    Message: {
-      Error: {
-        Title: t('MSG_GET_CURRENCIES'),
-        Message: t('MSG_GET_CURRENCIES_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    // TODO
-  })
-}
-
-const getCoins = () => {
-  coin.getCoins({
-    Message: {
-      Error: {
-        Title: t('MSG_GET_COINS'),
-        Message: t('MSG_GET_COINS_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    getCurrencies()
-  })
-}
-
-const getApplication = () => {
-  application.getApp({
-    AppID: AppID,
-    Message: {
-      Error: {
-        Title: t('MSG_GET_APP_FAIL'),
-        Popup: true,
-        Type: NotifyType.Error
-      }
-    }
-  }, () => {
-    // TODO
-  })
-}
-
 onMounted(() => {
-  getCoins()
+  if (coin.AppCoins.AppCoins.length === 0) {
+    getCoins(0, 100)
+  }
   if (good.AppGoods.AppGoods.length === 0) {
     getAppGoods(0, 500)
   }
@@ -87,4 +42,18 @@ const getAppGoods = (offset: number, limit: number) => {
   })
 }
 
+const getApplication = () => {
+  application.getApp({
+    AppID: AppID,
+    Message: {
+      Error: {
+        Title: t('MSG_GET_APP_FAIL'),
+        Popup: true,
+        Type: NotifyType.Error
+      }
+    }
+  }, () => {
+    // TODO
+  })
+}
 </script>
