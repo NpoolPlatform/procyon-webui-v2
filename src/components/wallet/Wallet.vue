51c982202faf2d46fa67062168e2b6dd9c4f9476
lidaqiang <dqli161@163.com>
2022-12-01 14:14:01 +0800

    update wallet

--- src/components/wallet/Wallet.vue
+++ src/components/wallet/Wallet.vue
@@ -22,77 +22,61 @@
 </template>
 
 <script setup lang="ts">
-import { Currency, NotificationType, SecondsEachDay, useCurrencyStore } from 'npool-cli-v2'
+import { SecondsEachDay } from 'npool-cli-v2'
 import {
   Account,
   AccountUsedFor,
+  General,
   NotifyType,
   TransferAccount,
   useAdminAppCoinStore,
+  useFrontendGeneralStore,
   useFrontendTransferAccountStore,
   useFrontendUserAccountStore
 } from 'npool-cli-v4'
 import { QAjaxBar } from 'quasar'
 import { getCoins } from 'src/api/chain'
 import { IntervalKey } from 'src/const/const'
-import { useLocalLedgerStore } from 'src/localstore/ledger'
-import { useGeneralStore } from 'src/teststore/mock/ledger'
-import { useLocalTransactionStore } from 'src/teststore/mock/transaction'
 import { defineAsyncComponent, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
-const Balance = defineAsyncComponent(
-  () => import('src/components/wallet/Balance.vue')
-)
-const Assets = defineAsyncComponent(
-  () => import('src/components/wallet/Assets.vue')
-)
-const Transactions = defineAsyncComponent(
-  () => import('src/components/wallet/Transactions.vue')
-)
-const WithdrawAddresses = defineAsyncComponent(
-  () => import('src/components/wallet/WithdrawAddresses.vue')
-)
-const WithdrawRecords = defineAsyncComponent(
-  () => import('src/components/wallet/WithdrawRecords.vue')
-)
-const TransferAccounts = defineAsyncComponent(
-  () => import('src/components/wallet/TransferAccounts.vue')
-)
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
 
-const localtrans = useLocalTransactionStore()
-const localledger = useLocalLedgerStore()
-const currency = useCurrencyStore()
+const Balance = defineAsyncComponent(() => import('src/components/wallet/Balance.vue'))
+const Assets = defineAsyncComponent(() => import('src/components/wallet/Assets.vue'))
+const Transactions = defineAsyncComponent(() => import('src/components/wallet/Transactions.vue'))
+const WithdrawAddresses = defineAsyncComponent(() => import('src/components/wallet/WithdrawAddresses.vue'))
+const WithdrawRecords = defineAsyncComponent(() => import('src/components/wallet/WithdrawRecords.vue'))
+const TransferAccounts = defineAsyncComponent(() => import('src/components/wallet/TransferAccounts.vue'))
+
+const general = useFrontendGeneralStore()
+const coin = useAdminAppCoinStore()
 const account = useFrontendUserAccountStore()
-const general = useGeneralStore()
+const transfer = useFrontendTransferAccountStore()
 
-const getWithdraws = (offset: number, limit: number) => {
-  localtrans.getWithdraws({
-    Offset: offset,
-    Limit: limit,
-    Message: {
-      Error: {
-        Title: t('MSG_GET_WITHDRAWS_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, (error: boolean, count?: number) => {
-    if (error) {
-      return
-    }
-    if (count === 0) {
-      localtrans.Withdraws.Withdraws = localtrans.Withdraws.Withdraws.sort((a, b) => {
-        return b.CreatedAt > a.CreatedAt ? -1 : 1
-      })
-      return
-    }
-    getWithdraws(limit + offset, limit)
-  })
-}
+onMounted(() => {
+  if (general.Generals.Generals.length === 0) {
+    getGenerals(0, 100)
+  }
+  if (general.getIntervalGeneralsByKey(IntervalKey.LastDay).length === 0) {
+    getIntervalGenerals(
+      IntervalKey.LastDay,
+      Math.ceil(new Date().getTime() / 1000) - SecondsEachDay,
+      Math.ceil(new Date().getTime() / 1000),
+      0, 100)
+  }
+  if (coin.AppCoins.AppCoins.length === 0) {
+    getCoins(0, 100)
+  }
+  if (account.UserAccounts.UserAccounts.length === 0) {
+    getUserAccounts(0, 100)
+  }
+  if (transfer.TransferAccounts.TransferAccounts.length === 0) {
+    getTransfers(0, 100)
+  }
+})
 
-const getUserGenerals = (offset:number, limit: number) => {
+const getGenerals = (offset:number, limit: number) => {
   general.getGenerals({
     Offset: offset,
     Limit: limit,
@@ -100,23 +84,14 @@ const getUserGenerals = (offset:number, limit: number) => {
       Error: {
         Title: t('MSG_GET_GENERAL_FAIL'),
         Popup: true,
-        Type: NotificationType.Error
+        Type: NotifyType.Error
       }
     }
-  }, (error: boolean, count?: number) => {
-    if (error) {
+  }, (error: boolean, rows: Array<General>) => {
+    if (error || rows.length < limit) {
       return
     }
-    if (count === 0) {
-      localledger.initGeneral(general.Generals.Generals)
-      getIntervalGenerals(
-        IntervalKey.LastDay,
-        Math.ceil(new Date().getTime() / 1000) - SecondsEachDay,
-        Math.ceil(new Date().getTime() / 1000),
-        0, 100)
-      return
-    }
-    getUserGenerals(limit + offset, limit)
+    getGenerals(limit + offset, limit)
   })
 }
 
@@ -130,88 +105,19 @@ const getIntervalGenerals = (key: IntervalKey, startAt: number, endAt: number, o
       Error: {
         Title: t('MSG_GET_GENERAL_FAIL'),
         Popup: true,
-        Type: NotificationType.Error
+        Type: NotifyType.Error
       }
     }
-  }, key, (error: boolean, count?: number) => {
-    if (error) {
-      return
-    }
-    if (count === 0) {
-      localledger.setLastDayGeneral(general.IntervalGenerals.get(key)?.Generals)
+  }, key, (error: boolean, rows: Array<General>) => {
+    if (error || rows.length < limit) {
       return
     }
     getIntervalGenerals(key, startAt, endAt, limit + offset, limit)
   })
 }
 
-const getCurrencies = () => {
-  currency.getAllCoinCurrencies({
-    Currencies: [Currency.USD],
-    Message: {
-      Error: {
-        Title: t('MSG_GET_CURRENCIES'),
-        Message: t('MSG_GET_CURRENCIES_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    // TODO
-  })
-}
-
-const getUserDetails = (offset: number, limit: number) => {
-  localtrans.getDetails({
-    Offset: offset,
-    Limit: limit,
-    EndAt: Math.ceil(Date.now() / 1000),
-    Message: {
-      Error: {
-        Title: t('MSG_GET_WITHDRAWS_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, (error: boolean, count?: number) => {
-    if (error) {
-      return
-    }
-    if (count === 0) {
-      return
-    }
-    getUserDetails(limit + offset, limit)
-  })
-}
-
-const transferAccount = useFrontendTransferAccountStore()
-const coin = useAdminAppCoinStore()
-
-onMounted(() => {
-  if (localtrans.Withdraws.Withdraws.length === 0) {
-    getWithdraws(0, 100)
-  }
-  if (general.Generals.Generals.length === 0 || localledger.Generals.size === 0) {
-    getUserGenerals(0, 100)
-  }
-
-  if (localtrans.Details.Details.length === 0) {
-    getUserDetails(0, 100)
-  }
-  if (transferAccount.TransferAccounts.TransferAccounts.length === 0) {
-    getTransferAccounts(0, 500)
-  }
-  if (account.UserAccounts.UserAccounts.length === 0) {
-    getUserAccounts(0, 500)
-  }
-  if (coin.AppCoins.AppCoins.length === 0) {
-    getCoins(0, 100)
-  }
-  getCurrencies()
-})
-
-const getTransferAccounts = (offset: number, limit: number) => {
-  transferAccount.getTransfers({
+const getTransfers = (offset: number, limit: number) => {
+  transfer.getTransfers({
     Offset: offset,
     Limit: limit,
     Message: {
@@ -225,7 +131,7 @@ const getTransferAccounts = (offset: number, limit: number) => {
     if (error || transfers.length < limit) {
       return
     }
-    getTransferAccounts(limit + offset, limit)
+    getTransfers(limit + offset, limit)
   })
 }
 
