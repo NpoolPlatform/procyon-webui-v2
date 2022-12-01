192562ed5cef7ae964e0e8f79acdbad20250657e
lidaqiang <dqli161@163.com>
2022-12-01 13:57:33 +0800

    update balance in wallet

--- src/components/wallet/Balance.vue
+++ src/components/wallet/Balance.vue
@@ -1,42 +1,46 @@
 <template>
-  <div v-if='!hidden'>
-    <h2>{{ $t('MSG_ACCOUNT_BALANCE') }}</h2>
-    <div class='earnings-summary'>
-      <div class='earnings-figure'>
-        <span class='amount'>{{ totalUSDTBalance.toFixed(4) }}</span>
-        <span class='unit'>{{ PriceCoinName }}</span>
-        <div class='hr' />
-        <h4 class='description'>
-          {{ $t('MSG_CURRENT_BALANCE') }} ({{ PriceCoinName }})
-        </h4>
-      </div>
-      <div class='earnings-figure'>
-        <span class='amount'>{{ totalJPYBalance.toFixed(4) }}</span>
-        <span class='unit'>JPY</span>
-        <div class='hr' />
-        <h4 class='description'>
-          {{ $t('MSG_CURRENT_BALANCE') }} (JPY)
-        </h4>
-      </div>
+  <h2>{{ $t('MSG_ACCOUNT_BALANCE') }}</h2>
+  <div class='earnings-summary'>
+    <div class='earnings-figure'>
+      <span class='amount'>{{ totalUSDTBalance.toFixed(4) }}</span>
+      <span class='unit'>{{ PriceCoinName }}</span>
+      <div class='hr' />
+      <h4 class='description'>
+        {{ $t('MSG_CURRENT_BALANCE') }} ({{ PriceCoinName }})
+      </h4>
+    </div>
+    <div class='earnings-figure'>
+      <span class='amount'>{{ totalJPYBalance.toFixed(4) }}</span>
+      <span class='unit'>JPY</span>
+      <div class='hr' />
+      <h4 class='description'>
+        {{ $t('MSG_CURRENT_BALANCE') }} (JPY)
+      </h4>
     </div>
   </div>
 </template>
 
 <script setup lang='ts'>
-import { PriceCoinName } from 'npool-cli-v2'
-import { useLocalLedgerStore } from 'src/localstore/ledger'
-import { defineProps, toRef, computed } from 'vue'
+import { PriceCoinName, useAdminCurrencyStore, useFrontendGeneralStore } from 'npool-cli-v4'
+import { computed } from 'vue'
 
-interface Props {
-  hidden?: boolean;
-}
+const general = useFrontendGeneralStore()
+const currency = useAdminCurrencyStore()
 
-const props = defineProps<Props>()
-const hidden = toRef(props, 'hidden')
+const totalUSDTBalance = computed(() => {
+  let total = 0
+  general.Generals.Generals.forEach((el) => {
+    total += Number(el.Spendable) * Number(currency.getCurrency(el.CoinTypeID))
+  })
+  return total
+})
 
-const localledger = useLocalLedgerStore()
-
-const totalUSDTBalance = computed(() => localledger.toUsdtAmount)
-const totalJPYBalance = computed(() => localledger.toJpyAmount)
+const totalJPYBalance = computed(() => {
+  let total = 0
+  general.Generals.Generals.forEach((el) => {
+    total += Number(el.Spendable) * Number(currency.getCurrency(el.CoinTypeID))
+  })
+  return total
+})
 
 </script>
