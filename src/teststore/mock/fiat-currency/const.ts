ab7c92482d0bb91474843f0877b04b9f2ef155d4
lidaqiang <dqli161@163.com>
2022-12-19 11:45:37 +0800

    add fiat currency api

---
+++ src/teststore/mock/fiat-currency/const.ts
@@ -0,0 +1,7 @@
+export enum API {
+  CREATE_FIATCURRENCYTYPE = '/chain/v1/create/fiatcurrencytype',
+  GET_COINFIATCURRENCIES = '/chain/v1/get/coinfiatcurrencies',
+  GET_FIATCURRENCYTYPES = '/chain/v1/get/fiatcurrencytypes',
+  GET_HISTORIES = '/chain/v1/get/histories',
+  UPDATE_FIATCURRENCYTYPE = '/chain/v1/update/fiatcurrencytype'
+}
