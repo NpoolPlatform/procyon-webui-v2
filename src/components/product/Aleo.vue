5a408ef8c19830739df4598aeedd24ebd15365ec
lidaqiang <dqli161@163.com>
2023-01-12 18:45:06 +0800

    update product page

--- src/components/product/Aleo.vue
+++ src/components/product/Aleo.vue
@@ -74,73 +74,7 @@
       </div>
     </template>
     <template #product-detail>
-      <div v-show='coinDescription'>
-        <h3>{{ coinDescription ? $t(coinDescription?.Title) : '' }}</h3>
-        <p v-html='coinDescription ? $t(coinDescription?.Message) : ""' />
-      </div>
-      <h3>{{ $t('MSG_VC_INVESTMENT') }}</h3>
-      <p v-html='$t("MSG_VC_INVESTMENT_CAPTION")' />
-      <div class='logo-display'>
-        <div><img src='vc-logos/softbank-vision-fund.png'></div>
-        <div><img src='vc-logos/a16z.png'></div>
-        <div><img src='vc-logos/polychain-capital.png'></div>
-        <div><img src='vc-logos/coinbase.png'></div>
-        <div><img src='vc-logos/samsung-next.png'></div>
-        <div><img src='vc-logos/slow-ventures.png'></div>
-        <div><img src='vc-logos/dekrypt-capital.png'></div>
-        <div><img src='vc-logos/kora.png'></div>
-        <div><img src='vc-logos/sea-capital.png'></div>
-        <div><img src='vc-logos/tiger-global.png'></div>
-        <div><img src='vc-logos/zkv.png'></div>
-        <div><img src='vc-logos/placeholder.png'></div>
-        <div><img src='vc-logos/scalar-capital.png'></div>
-        <div><img src='vc-logos/galaxy-digital.png'></div>
-        <div><img src='vc-logos/variant.png'></div>
-        <div><img src='vc-logos/ethereal-ventures.png'></div>
-        <div><img src='vc-logos/a_capital.png'></div>
-        <div><img src='vc-logos/david-namdar.png'></div>
-        <div><img src='vc-logos/balaji-srinivasan.png'></div>
-      </div>
-      <h3>{{ $t('MSG_WHY_ALEO_IMPORTANT_TO_WEB3') }}</h3>
-      <p v-html='$t("MSG_WHY_ALEO_IMPORTANT_TO_WEB3_CAPTION")' />
-      <h3>{{ $t('MSG_ALEO_TEAM') }}</h3>
-      <p v-html='$t("MSG_ALEO_TEAM_CAPTION")' />
-      <div class='project-team'>
-        <div v-for='member in members' :key='member.Name' class='team-member'>
-          <h4 class='name'>
-            {{ member.Name }}
-          </h4>
-          <h5 class='title'>
-            {{ $t(member.Title) }}
-          </h5>
-          <p class='bio' v-html='$t(member.Background)' />
-        </div>
-      </div>
-      <h3>{{ $t('MSG_ALEO_TOKENOMICS') }}</h3>
-      <p><img class='project-promo-content' :src='locale === "ja-JP" ? "product/aleo/aleo-posw-jp.jpg" : "product/aleo/aleo-posw-en.jpg"'></p>
-      <p>
-        <a href='https://coingenius.news/tokenomics-aleo/'>{{ $t('MSG_ALEO_TOKENOMICS_COINGENIUS_NEWS') }}</a>
-      </p>
-      <p v-html='$t("MSG_ALEO_TOKENOMICS_CAPTION")' />
-      <h3>{{ $t('MSG_ALEO_VS_COMPETITORS') }}</h3>
-      <p v-html='$t("MSG_ALEO_VS_COMPETITORS_CAPTION")' />
-      <p><img class='project-promo-content' :src='locale === "ja-JP" ? "product/aleo/aleo-comparison-jp.png" : "product/aleo/aleo-comparison-en.png"'></p>
-      <h4>{{ $t('MSG_ZK_PROOF_PRIVACY') }}</h4>
-      <p v-html='$t("MSG_ZK_PROOF_PRIVACY_CAPTION")' />
-      <p><img class='project-promo-content' :src='locale === "ja-JP" ? "product/aleo/aleo-algorithm-jp.png" : "product/aleo/aleo-algorithm-en.png"'></p>
-      <h3>{{ $t('MSG_ALEO_FUTURE') }}</h3>
-      <p v-html='$t("MSG_ALEO_FUTURE_CAPTION_1")' />
-      <div class='logo-display'>
-        <div><img src='vc-logos/coinbase.png'></div>
-        <div><img src='vc-logos/gemini.png'></div>
-        <div><img src='vc-logos/kraken.png'></div>
-        <div><img src='vc-logos/binance.png'></div>
-        <div><img src='vc-logos/huobi.png'></div>
-        <div><img src='vc-logos/okex.png'></div>
-      </div>
-      <p v-html='$t("MSG_ALEO_FUTURE_CAPTION_2")' />
-      <h3>{{ $t('MSG_RISK_WARNING_AND_DISCLAIMER') }}</h3>
-      <p v-html='$t("MSG_RISK_WARNING_AND_DISCLAIMER_CAPTION")' />
+      <ProductDetail />
     </template>
     <template #sidebar>
       <div id='project-links' class='product-sidebar-section'>
@@ -201,20 +135,21 @@ import { useI18n } from 'vue-i18n'
 import question from '../../assets/question.svg'
 import lightbulb from '../../assets/lightbulb.svg'
 import { DefaultGoodID } from 'src/const/const'
-import { AppGood, NotifyType, useAdminAppGoodStore, useAdminCoinDescriptionStore, CoinDescriptionUsedFor, useAdminCurrencyStore } from 'npool-cli-v4'
+import { AppGood, NotifyType, useAdminAppGoodStore, useAdminCoinDescriptionStore, useAdminCurrencyStore } from 'npool-cli-v4'
 import { getCurrencies, getDescriptions } from 'src/api/chain'
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
 
 const ProductPage = defineAsyncComponent(() => import('src/components/product/ProductPage.vue'))
+const ProductDetail = defineAsyncComponent(() => import('src/components/product/detail/ja-JP/Detail.vue'))
 
 interface Query {
   goodId: string;
   purchaseAmount: number;
 }
 
-const { locale } = useI18n({ useScope: 'global' })
+// const { locale } = useI18n({ useScope: 'global' })
 
 const route = useRoute()
 const query = computed(() => route.query as unknown as Query)
@@ -227,33 +162,7 @@ const target = computed(() => good.getGoodByID(goodID.value) as AppGood)
 const currency = useAdminCurrencyStore()
 
 const description = useAdminCoinDescriptionStore()
-const coinDescription = computed(() => description.getCoinDescriptionByCoinUsedFor(target.value?.CoinTypeID, CoinDescriptionUsedFor.ProductPage))
-
-interface Member {
-  Name: string;
-  Title: string;
-  Background: string;
-}
-
-const members = computed(() => [
-  {
-    Name: 'Howard Wu',
-    Title: t('MSG_CO_FOUNDER'),
-    Background: t('MSG_HOWARD_WU_BACKGROUND')
-  }, {
-    Name: 'Collin Chin',
-    Title: t('MSG_CO_FOUNDER'),
-    Background: t('MSG_COLLIN_CHIN_BACKGROUND')
-  }, {
-    Name: 'Raymond Chu',
-    Title: t('MSG_CO_FOUNDER'),
-    Background: t('MSG_RAYMOND_CHU_BACKGROUND')
-  }, {
-    Name: 'Michael Beller',
-    Title: t('MSG_CFO'),
-    Background: t('MSG_MICHAEL_BELLER_BACKGROUND')
-  }
-] as Array<Member>)
+// const coinDescription = computed(() => description.getCoinDescriptionByCoinUsedFor(target.value?.CoinTypeID, CoinDescriptionUsedFor.ProductPage))
 
 onMounted(() => {
   good.getAppGood({
