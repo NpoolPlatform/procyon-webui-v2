bbef80c47cdff0483e3df847814dc5097180df24
lidaqiang <dqli161@163.com>
2023-01-31 10:25:28 +0800

    display multi products

--- src/components/dashboard/MiningCards.vue
+++ src/components/dashboard/MiningCards.vue
@@ -21,29 +21,17 @@ const currency = useAdminCurrencyStore()
 const coin = useAdminAppCoinStore()
 
 const profit = useFrontendProfitStore()
-const goodProfits = computed(() => {
-  const data = Array.from(profit.GoodProfits.GoodProfits).map((el) => {
-    return {
-      ...el,
-      Units: profit.getPurchaseUnits(el.CoinTypeID),
-      CoinPreSale: coin.preSale(el.CoinTypeID),
-      TotalInComing: profit.getTotalIncoming(el.CoinTypeID),
-      TotalUSDInComing: currency.getUSDCurrency(el.CoinTypeID) * profit.getTotalIncoming(el.CoinTypeID),
-      Last24HoursInComing: profit.getIntervalGoodProfitInComing(IntervalKey.LastDay, el.CoinTypeID),
-      Last24HoursUSDInComing: currency.getUSDCurrency(el.CoinTypeID) * profit.getIntervalGoodProfitInComing(IntervalKey.LastDay, el.CoinTypeID),
-      Last30DaysInComing: profit.getIntervalGoodProfitInComing(IntervalKey.LastMonth, el.CoinTypeID),
-      Last30DaysUSDInComing: currency.getUSDCurrency(el.CoinTypeID) * profit.getIntervalGoodProfitInComing(IntervalKey.LastMonth, el.CoinTypeID)
-    } as MyGoodProfit
-  })
-  const coinTypeIDs = [] as Array<string>
-  return data?.filter((el) => {
-    const index = coinTypeIDs.findIndex((coinTypeID) => coinTypeID === el.CoinTypeID)
-    if (index === -1) {
-      coinTypeIDs.push(el.CoinTypeID)
-      return true
-    } else {
-      return false
-    }
-  })
-})
+const goodProfits = computed(() => Array.from(profit.GoodProfits.GoodProfits).map((el) => {
+  return {
+    ...el,
+    Units: profit.getPurchaseUnits(el.CoinTypeID),
+    CoinPreSale: coin.preSale(el.CoinTypeID),
+    TotalInComing: profit.getTotalIncoming(el.CoinTypeID),
+    TotalUSDInComing: currency.getUSDCurrency(el.CoinTypeID) * profit.getTotalIncoming(el.CoinTypeID),
+    Last24HoursInComing: profit.getIntervalGoodProfitInComing(IntervalKey.LastDay, el.CoinTypeID),
+    Last24HoursUSDInComing: currency.getUSDCurrency(el.CoinTypeID) * profit.getIntervalGoodProfitInComing(IntervalKey.LastDay, el.CoinTypeID),
+    Last30DaysInComing: profit.getIntervalGoodProfitInComing(IntervalKey.LastMonth, el.CoinTypeID),
+    Last30DaysUSDInComing: currency.getUSDCurrency(el.CoinTypeID) * profit.getIntervalGoodProfitInComing(IntervalKey.LastMonth, el.CoinTypeID)
+  } as MyGoodProfit
+}))
 </script>
