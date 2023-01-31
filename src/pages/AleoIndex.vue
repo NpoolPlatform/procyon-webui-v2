d6760ffbdd6a06b45c905238b70c15fcce3a6496
lidaqiang <dqli161@163.com>
2023-01-31 15:23:59 +0800

    add partners section

--- src/pages/AleoIndex.vue
+++ src/pages/AleoIndex.vue
@@ -5,10 +5,12 @@
   <WhyMe />
   <div v-if='locale === "en-US"'>
     <WhatWeKnowUS />
+    <PartnersUS />
     <WhatMakesUS />
   </div>
   <div v-else>
     <WhatMakesJP />
+    <PartnersJP />
     <WhatWeKnowJP />
   </div>
   <SupportCenter id='support' />
@@ -25,8 +27,10 @@ const ProductSummary = defineAsyncComponent(() => import('src/components/index/P
 const MixProducts = defineAsyncComponent(() => import('src/components/index/MixProducts.vue'))
 const WhyMe = defineAsyncComponent(() => import('src/components/index/WhyMeV2.vue'))
 const WhatWeKnowUS = defineAsyncComponent(() => import('src/components/index/en-US/WhatWeKnow.vue'))
+const PartnersUS = defineAsyncComponent(() => import('src/components/index/en-US/Partners.vue'))
 const WhatWeKnowJP = defineAsyncComponent(() => import('src/components/index/ja-JP/WhatWeKnow.vue'))
 const WhatMakesUS = defineAsyncComponent(() => import('src/components/index/en-US/WhatMakes.vue'))
+const PartnersJP = defineAsyncComponent(() => import('src/components/index/ja-JP/Partners.vue'))
 const WhatMakesJP = defineAsyncComponent(() => import('src/components/index/ja-JP/WhatMakes.vue'))
 const SupportCenter = defineAsyncComponent(() => import('src/components/help/FAQMenu.vue'))
 
