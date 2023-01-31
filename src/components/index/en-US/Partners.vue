63a6f9fbdeb7ea745b1bb7642392be18b5acb777
lidaqiang <dqli161@163.com>
2023-01-31 15:23:59 +0800

    add partners section

---
+++ src/components/index/en-US/Partners.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class='content'>
+    <h1 id='partners'>
+      Who are Procyon's Partners?
+    </h1>
+
+    <img src='../../../assets/partners-diagram-en.svg'>
+
+    <h2>Hpool</h2>
+
+    <div class='logo-display'>
+      <a href='https://www.hpool.in/' target='_blank'><img src='vc-logos/hpool.png'></a>
+    </div>
+
+    <p>Founded in 2017, Hpool is the world's largest PoC (Proof of Consensus) mining pool operating 150,000 mining machines, serving nearly 300,000 users worldwide, and managing hundreds of millions of dollars in assets.</p>
+
+    <p>Hpool is most famous for dominating over 80% network hashpower on the Chia blockchain 1 month after its mainnet launch.</p>
+
+    <p>Large-scale mining pools such as Hpool provide peak return on investment, but offer first-batch mining opportunities only to institutional investors. However, as Hpool is Procyon’s official mining partner, they have opened their doors to our community, regardless of investment size, giving our community members an extremely rare and exclusive opportunity to participate in Hpool’s world-class mining operations.</p>
+
+    <h2>Inazuma Capital</h2>
+
+    <div class='logo-display'>
+      <a href='https://inazuma-capital.co/' target='_blank'><img src='vc-logos/inazuma-dark.png'></a>
+    </div>
+
+    <p>Founded in 2020, Inazuma Capital is Japan's first crypto asset venture capital firm that backs Web3-centric entrepreneurs focusing on building the future of the blockchain space. We invest in seed to private to late-stage technology companies, across several traditional, as well as emerging sectors including Infrastructure, crypto enterprise, gaming, and NFT. <strong>Inazuma Capital currently has $30M in assets under management backed by Japan's most prolific investors</strong>.</p>
+
+    <p>Inazuma Capital is Procyon’s VC arm and is responsible for conducting research and due diligence, brokering partnerships with top-class projects, and delivering exclusive investment opportunities for our community.</p>
+  </div>
+
+  <div class='hr' />
+</template>
