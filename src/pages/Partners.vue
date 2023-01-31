7860ed55be83bd934f8cfd663a42c10eb71c7b47
lidaqiang <dqli161@163.com>
2023-01-31 17:09:15 +0800

    add partners jump

---
+++ src/pages/Partners.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if='locale === "en-US"'>
+    <PartnersUS />
+  </div>
+  <div v-else>
+    <PartnersJP />
+  </div>
+</template>
+<script lang='ts' setup>
+import { defineAsyncComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { locale } = useI18n({ useScope: 'global' })
+const PartnersUS = defineAsyncComponent(() => import('src/components/index/en-US/Partners.vue'))
+const PartnersJP = defineAsyncComponent(() => import('src/components/index/ja-JP/Partners.vue'))
+</script>
