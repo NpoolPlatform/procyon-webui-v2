1f73fc897addb726682583b9c3d4d02015308890
lidaqiang <dqli161@163.com>
2023-01-31 12:06:13 +0800

    add resource page

---
+++ src/pages/Resources.vue
@@ -0,0 +1,10 @@
+<template>
+  <Resources />
+</template>
+
+<script setup lang='ts'>
+import { defineAsyncComponent } from 'vue'
+
+const Resources = defineAsyncComponent(() => import('src/components/resource/Resources.vue'))
+
+</script>
