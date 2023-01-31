cbeedd4d18ffd2aad58499bb1465869414ed9fbe
lidaqiang <dqli161@163.com>
2023-01-31 12:22:12 +0800

    update resource page

--- src/pages/Resources.vue
+++ src/pages/Resources.vue
@@ -1,10 +1,18 @@
 <template>
-  <Resources />
+  <div v-if='locale === "en-US"'>
+    <ResourcesEN />
+  </div>
+  <div else>
+    <ResourcesJP />
+  </div>
 </template>
 
 <script setup lang='ts'>
 import { defineAsyncComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-const Resources = defineAsyncComponent(() => import('src/components/resource/Resources.vue'))
+const { locale } = useI18n({ useScope: 'global' })
+const ResourcesEN = defineAsyncComponent(() => import('src/components/resource/en-US/Resources.vue'))
+const ResourcesJP = defineAsyncComponent(() => import('src/components/resource/ja-JP/Resources.vue'))
 
 </script>
