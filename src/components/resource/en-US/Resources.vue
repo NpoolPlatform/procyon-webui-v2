b6d5236cdcb1b4a8b5a7fdb46f297ad50ebd724a
lidaqiang <dqli161@163.com>
2023-01-31 12:22:12 +0800

    update resource page

---
+++ src/components/resource/en-US/Resources.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class='content'>
+    <h2>Resources &amp; Manuals</h2>
+
+    <h3>General Info</h3>
+
+    <ul class='resource-center'>
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/11cTZKtlwTn1gWnRge6fp10TRrvh-dgIf/view?usp=share_link' target='_blank'>Procyon Platform General Manual</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1DuZm_aDiqojpA6VlDq_z4Dt3JTi44XVe/view?usp=share_link' target='_blank'>How to Purchase Aleo Mining Products</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/c4uPn8bU-T0' target='_blank'>How to Purchase Aleo Mining Products</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1TeFrwlHbkqTtsq2dzhW6HuWVg_FHbpwr/view?usp=share_link' target='_blank'>How to Transfer Mining Rewards and Wallet Balance</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/IGtH51asH88' target='_blank'>How to Transfer Mining Rewards and Wallet Balance</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1hXt-5S19etkjoyh1EqVSFySLt6ME-Knc/view?usp=share_link' target='_blank'>How to Deposit Assets into Your Procyon Wallet</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/LZbpVdEmzzY' target='_blank'>How to Deposit Assets into Your Procyon Wallet</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1TYpEcTBoqV38CsCcJ2knw2EImi7dpggJ/view?usp=share_link' target='_blank'>How to Externally Transfer Your Assets</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/IGtH51asH88' target='_blank'>How to Externally Transfer Your Assets</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1AP69mn8nMWMtJkWgOUTZcOT0UrXp1Vgz/view?usp=share_link' target='_blank'>How to Transfer Assets to Another Procyon Account</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1s2jqX76QaRlltLcEe2ayeHZdTYQm4AFf/view?usp=share_link' target='_blank'>Support &amp; FAQ</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1OxKI2g4G83SUGp9ZDps9r8AAMsx81U8-/view?usp=share_link' target='_blank'>If You Cannot Access Emails from Procyon (Gmail)</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/ZxGKm-B9vU4' target='_blank'>How to the Check Procyon Blog</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1vODQaAZxSqv5SZaDL4quF0laXD-M54Sa/view?usp=share_link' target='_blank'>Mining Terminologies</a>
+      </li>
+    </ul>
+
+    <h3>Login</h3>
+
+    <ul class='resource-center'>
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1otpSmbJDje3Lt64XrUYt1jSZ0OhlZ4Sk/view?usp=share_link' target='_blank'>Forgot Your Password?</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/8RkDiUaafb4' target='_blank'>Forgot Your Password?</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1aoNOumwbMMoB9KKEwvYMkVpMFBa_wjEt/view?usp=share_link' target='_blank'>How to Log in</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/c5kwRniqR4w' target='_blank'>How to Log in</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1GahESamBwv7oCtFjizir8Q19F5lRSi6_/view?usp=share_link' target='_blank'>How to Login for the First Time</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/8RkDiUaafb4' target='_blank'>How to Login for the First Time</a>
+      </li>
+    </ul>
+
+    <h3>Dashboard</h3>
+
+    <ul class='resource-center'>
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1UwNYdUL3tVyQq8xeDocx3yO12K7khzLB/view?usp=share_link' target='_blank'>How to Check Your Mining Rewards</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1ALtS3UrHNSB-hp7LBxXSiyCztqNCWUsD/view?usp=share_link' target='_blank'>How to Check Your Order History</a>
+      </li>
+    </ul>
+
+    <h3>Account Settings</h3>
+
+    <ul class='resource-center'>
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1tUF7emxMbfVUkeZkXb-7i_653UNCui-3/view?usp=share_link' target='_blank'>How to Set Up 2 Factor Verification</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/eYO5GNUjMgs' target='_blank'>How to Set Up 2 Factor Verification</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1T3gYCMOMsiRGneuqYhUNh4R5GzVGIaJ1/view?usp=share_link' target='_blank'>How to Set Up Login Using Google Authentication</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/HIA060jno1E' target='_blank'>How to Set Up Login Using Google Authentication</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1bJDv-K-UTn22XnfhpPyokHRwrqNRVEtA/view?usp=share_link' target='_blank'>How to Change Your Personal Information</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/CKNDVnxc9r4' target='_blank'>How to Change Your Personal Information</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1hDYVQStVh-Db5t-ezrRE2idsjgOkdTKk/view?usp=share_link' target='_blank'>How to Change Your Password</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/D5V9IwkaDQs' target='_blank'>How to Change Your Password</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/17UTgRv4VbXdC-odofNDjkldQPDCQ3J50/view?usp=share_link' target='_blank'>How to Set Up Your Phone Number</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/DdvOzdjYCsM' target='_blank'>How to Set Up Your Phone Number</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1dgOkWLpPTdiku6M3M3O0JDciUrwvEYTi/view?usp=share_link' target='_blank'>How to Change Your Email Address</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1-eiggi9Kozwk8C6YeKyE0nlnHnGWZRco/view?usp=share_link' target='_blank'>How to Change Your Phone Number</a>
+      </li>
+    </ul>
+
+    <h3>KYC</h3>
+
+    <ul class='resource-center'>
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1eh-9urmyzF0srpQKmKq2aepWb0h9awox/view?usp=share_link' target='_blank'>How to Submit KYC</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/Rhc-uyRe2uw' target='_blank'>How to Submit KYC</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1w-P1WTkuWBDxcIaxvwNLYReL4mFh08ro/view?usp=share_link' target='_blank'>How to Confirm Your KYC Submission Status</a>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/components/resource/ja-JP/Resources.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class='content'>
+    <h2>マニュアル</h2>
+
+    <h3>総合案内</h3>
+
+    <ul class='resource-center'>
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/11cTZKtlwTn1gWnRge6fp10TRrvh-dgIf/view?usp=share_link' target='_blank'>Procyonプラットフォーム総合ユーザーガイド</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1DuZm_aDiqojpA6VlDq_z4Dt3JTi44XVe/view?usp=share_link' target='_blank'>Aleoマイニング製品の購入方法</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/c4uPn8bU-T0' target='_blank'>Aleoマイニング製品の購入方法</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1TeFrwlHbkqTtsq2dzhW6HuWVg_FHbpwr/view?usp=share_link' target='_blank'>マイニング報酬およびProcyonウォレット残高の送金方法</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/IGtH51asH88' target='_blank'>マイニング報酬およびProcyonウォレット残高の送金方法</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1hXt-5S19etkjoyh1EqVSFySLt6ME-Knc/view?usp=share_link' target='_blank'>Procyonウォレットへの入金方法</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/LZbpVdEmzzY' target='_blank'>Procyonウォレットへの入金方法</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1TYpEcTBoqV38CsCcJ2knw2EImi7dpggJ/view?usp=share_link' target='_blank'>外部への送金方法</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/IGtH51asH88' target='_blank'>外部への送金方法</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1AP69mn8nMWMtJkWgOUTZcOT0UrXp1Vgz/view?usp=share_link' target='_blank'>Procyonアカウント間の送金方法</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1s2jqX76QaRlltLcEe2ayeHZdTYQm4AFf/view?usp=share_link' target='_blank'>サポートFAQ</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1OxKI2g4G83SUGp9ZDps9r8AAMsx81U8-/view?usp=share_link' target='_blank'>GmailでProcyonのメールが確認できない場合</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/ZxGKm-B9vU4' target='_blank'>ブログの確認方法</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1vODQaAZxSqv5SZaDL4quF0laXD-M54Sa/view?usp=share_link' target='_blank'>マイニングに関する用語の説明</a>
+      </li>
+    </ul>
+
+    <h3>ログイン</h3>
+
+    <ul class='resource-center'>
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1otpSmbJDje3Lt64XrUYt1jSZ0OhlZ4Sk/view?usp=share_link' target='_blank'>パスワードを忘れた場合</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/8RkDiUaafb4' target='_blank'>パスワードを忘れた場合</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1aoNOumwbMMoB9KKEwvYMkVpMFBa_wjEt/view?usp=share_link' target='_blank'>ログイン方法 </a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/c5kwRniqR4w' target='_blank'>ログイン方法 </a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1GahESamBwv7oCtFjizir8Q19F5lRSi6_/view?usp=share_link' target='_blank'>初回ログインの方法</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/8RkDiUaafb4' target='_blank'>初回ログインの方法</a>
+      </li>
+    </ul>
+
+    <h3>ダッシュボード</h3>
+
+    <ul class='resource-center'>
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1UwNYdUL3tVyQq8xeDocx3yO12K7khzLB/view?usp=share_link' target='_blank'>マイニング報酬の確認方法</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1ALtS3UrHNSB-hp7LBxXSiyCztqNCWUsD/view?usp=share_link' target='_blank'>注文履歴の確認方法</a>
+      </li>
+    </ul>
+
+    <h3>アカウント情報</h3>
+
+    <ul class='resource-center'>
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1tUF7emxMbfVUkeZkXb-7i_653UNCui-3/view?usp=share_link' target='_blank'>2段階認証の設定</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/eYO5GNUjMgs' target='_blank'>2段階認証の設定</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1T3gYCMOMsiRGneuqYhUNh4R5GzVGIaJ1/view?usp=share_link' target='_blank'>Google認証のログイン設定</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/HIA060jno1E' target='_blank'>Google認証のログイン設定</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1bJDv-K-UTn22XnfhpPyokHRwrqNRVEtA/view?usp=share_link' target='_blank'>アカウント情報の変更</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/CKNDVnxc9r4' target='_blank'>アカウント情報の変更</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1hDYVQStVh-Db5t-ezrRE2idsjgOkdTKk/view?usp=share_link' target='_blank'>パスワードの変更方法</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/D5V9IwkaDQs' target='_blank'>パスワードの変更方法</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/17UTgRv4VbXdC-odofNDjkldQPDCQ3J50/view?usp=share_link' target='_blank'>電話番号の設定</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/DdvOzdjYCsM' target='_blank'>電話番号の設定</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1dgOkWLpPTdiku6M3M3O0JDciUrwvEYTi/view?usp=share_link' target='_blank'>電話番号の設定</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1-eiggi9Kozwk8C6YeKyE0nlnHnGWZRco/view?usp=share_link' target='_blank'>携帯電話の変更方法</a>
+      </li>
+    </ul>
+
+    <h3>KYC (本人確認)</h3>
+
+    <ul class='resource-center'>
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1eh-9urmyzF0srpQKmKq2aepWb0h9awox/view?usp=share_link' target='_blank'>KYC申請手順ガイド</a>
+      </li>
+
+      <li class='vid-icon'>
+        <a href='https://youtu.be/Rhc-uyRe2uw' target='_blank'>KYC申請手順ガイド</a>
+      </li>
+
+      <li class='pdf-icon'>
+        <a href='https://drive.google.com/file/d/1w-P1WTkuWBDxcIaxvwNLYReL4mFh08ro/view?usp=share_link' target='_blank'>審査状況の確認方法</a>
+      </li>
+    </ul>
+  </div>
+</template>
