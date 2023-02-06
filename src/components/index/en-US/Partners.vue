6ea1278f6cb6e56869ff4822cd016a9584bb7678
lidaqiang <dqli161@163.com>
2023-02-06 12:36:34 +0800

    bugfix: #1173

--- src/components/index/en-US/Partners.vue
+++ src/components/index/en-US/Partners.vue
@@ -14,7 +14,9 @@
 
     <p>Founded in 2017, Hpool is the world's largest PoC (Proof of Consensus) mining pool operating 150,000 mining machines, serving nearly 300,000 users worldwide, and managing hundreds of millions of dollars in assets.</p>
 
-    <p>Hpool is most famous for dominating over 80% network hashpower on the Chia blockchain 1 month after its mainnet launch.</p>
+    <p>
+      <strong>Hpool is most famous for dominating over 80% network hashpower on the Chia blockchain 1 month after its mainnet launch.</strong>
+    </p>
 
     <p>Large-scale mining pools such as Hpool provide peak return on investment, but offer first-batch mining opportunities only to institutional investors. However, as Hpool is Procyon’s official mining partner, they have opened their doors to our community, regardless of investment size, giving our community members an extremely rare and exclusive opportunity to participate in Hpool’s world-class mining operations.</p>
 
