ce3c6fc3e2c7e7a0a98856277287f1cf885939ba
lidaqiang <dqli161@163.com>
2023-02-07 14:06:31 +0800

    add notif api

---
+++ src/teststore/mock/notify/types.ts
@@ -0,0 +1,38 @@
+import { BaseRequest } from 'npool-cli-v4'
+import { Notif } from './const'
+
+export interface GetNotifRequest extends BaseRequest{
+  ID: string;
+}
+
+export interface GetNotifResponse {
+  Info: Notif;
+}
+
+export interface GetNotifsRequest extends BaseRequest {
+  /** @format int64 */
+  Offset: number;
+  /** @format int64 */
+  Limit: number;
+}
+
+export interface GetNotifsResponse {
+  Infos: Notif[];
+  /** @format int64 */
+  Total: number;
+}
+
+export interface GetAppUserNotifsRequest extends BaseRequest {
+  TargetAppID: string;
+  TargetUserID: string;
+  /** @format int64 */
+  Offset: number;
+  /** @format int64 */
+  Limit: number;
+}
+
+export interface GetAppUserNotifsResponse {
+  Infos: Notif[];
+  /** @format int64 */
+  Total: number;
+}
