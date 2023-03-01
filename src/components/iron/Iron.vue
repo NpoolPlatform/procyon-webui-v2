4a2352d8734a291fbc0d07259f0ace5ab318d632
lidaqiang <dqli161@163.com>
2023-03-01 10:50:18 +0800

    add iron page detail

--- src/components/iron/Iron.vue
+++ src/components/iron/Iron.vue
@@ -2,15 +2,15 @@
   <ProductPage
     :good-id='goodID'
     :purchase-amount='purchaseAmount'
-    project-class='project-aleo'
-    bg-img='product/aleo/aleo-banner.jpg'
+    project-class='project-iron-fish'
+    bg-img='product/iron/iron-fish-banner.jpg'
     :customize-info='true'
   >
     <template #product-info>
       <div class='three-section'>
         <h4>{{ $t('MSG_PRICE') }}:</h4>
         <span class='number'>{{ good.getPrice(goodID) }}</span>
-        <span class='unit'>{{ PriceCoinName }}</span>
+        <span class='unit'>{{ PriceCoinName }} / UNIT</span>
         <div class='tooltip'>
           <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE') }}</span>
           <p class='tooltip-text'>
@@ -89,46 +89,13 @@
           {{ $t('MSG_PROJECT_LINKS') }}
         </h3>
         <ul class='product-links'>
-          <li>
-            <a href='https://www.aleo.org/' target='_blank'><img class='link-icon' src='font-awesome/homepage.svg'>
-              <span>{{ $t('MSG_ALEO_HOMEPAGE') }}</span>
-            </a>
-          </li>
-          <li>
-            <a href='https://twitter.com/AleoHQ' target='_blank'><img class='link-icon' src='font-awesome/twitter.svg'>
-              <span>{{ $t('MSG_TWITTER') }}</span>
-            </a>
-          </li>
-          <li>
-            <a href='https://www.youtube.com/channel/UCS_HKT2heOC_q88YQLiJt0g' target='_blank'><img class='link-icon' src='font-awesome/youtube.svg'>
-              <span>{{ $t('MSG_YOUTUBE') }}</span>
-            </a>
-          </li>
-          <li>
-            <a href='https://discord.com/invite/aleohq' target='_blank'><img class='link-icon' src='font-awesome/discord.svg'>
-              <span>{{ $t('MSG_DISCORD') }}</span>
-            </a>
-          </li>
-          <li>
-            <a href='https://github.com/AleoHQ' target='_blank'><img class='link-icon' src='font-awesome/github.svg'>
-              <span>{{ $t('MSG_GITHUB') }}</span>
-            </a>
-          </li>
+          <li><a href='https://www.ironfish.network/' target='_blank'><img class='link-icon' src='font-awesome/homepage.svg'><span>{{ $t('MSG_IRON_FISH_HOMEPAGE') }}</span></a></li>
+          <li><a href='https://twitter.com/ironfishcrypto' target='_blank'><img class='link-icon' src='font-awesome/twitter.svg'><span>{{ $t('MSG_TWITTER') }}</span></a></li>
+          <li><a href='https://t.me/ironfishcryptochat' target='_blank'><img class='link-icon' src='font-awesome/telegram.svg'><span>{{ $t('MSG_TELEGRAM') }}</span></a></li>
+          <li><a href='https://discord.ironfish.network/' target='_blank'><img class='link-icon' src='font-awesome/discord.svg'><span>{{ $t('MSG_DISCORD') }}</span></a></li>
+          <li><a href='https://github.com/iron-fish' target='_blank'><img class='link-icon' src='font-awesome/github.svg'><span>{{ $t('MSG_GITHUB') }}</span></a></li>
         </ul>
       </div>
-      <!-- <div id='help' class='product-sidebar-section'>
-        <h3 class='form-title'>
-          {{ $t('MSG_PRODUCT_FAQ') }}
-        </h3>
-        <ul class='product-links'>
-          <li>
-            <a href='https://drive.google.com/file/d/1DuZm_aDiqojpA6VlDq_z4Dt3JTi44XVe/view?usp=share_link'>
-              <img class='link-icon' :src='lightbulb'>
-              <span>{{ $t('MSG_PDF_MANUAL') }}</span>
-            </a>
-          </li>
-        </ul>
-      </div> -->
     </template>
   </ProductPage>
 </template>
@@ -148,9 +115,9 @@ import { getCurrencies, getDescriptions } from 'src/api/chain'
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t, locale } = useI18n({ useScope: 'global' })
 
-const ProductPage = defineAsyncComponent(() => import('src/components/product/ProductPage.vue'))
-const ProductDetailUS = defineAsyncComponent(() => import('src/components/product/en-US/Detail.vue'))
-const ProductDetailJP = defineAsyncComponent(() => import('src/components/product/ja-JP/Detail.vue'))
+const ProductPage = defineAsyncComponent(() => import('src/components/iron/IronProductPage.vue'))
+const ProductDetailUS = defineAsyncComponent(() => import('src/components/iron/en-US/Detail.vue'))
+const ProductDetailJP = defineAsyncComponent(() => import('src/components/iron/ja-JP/Detail.vue'))
 
 interface Query {
   goodId: string;
@@ -168,7 +135,6 @@ const target = computed(() => good.getGoodByID(goodID.value) as AppGood)
 const currency = useAdminCurrencyStore()
 
 const description = useAdminCoinDescriptionStore()
-// const coinDescription = computed(() => description.getCoinDescriptionByCoinUsedFor(target.value?.CoinTypeID, CoinDescriptionUsedFor.ProductPage))
 
 onMounted(() => {
   good.getAppGood({
