dac5c75d8fb50c437a4059aeb53d04c73915c5c4
lidaqiang <dqli161@163.com>
2023-03-06 15:56:22 +0800

    add export mining rewards

--- src/components/dashboard/MiningCard.vue
+++ src/components/dashboard/MiningCard.vue
@@ -78,7 +78,7 @@
       <button :class='[ "alt show-more", short ? "" : "open" ]' @click='onExpandClick'>
         <img :src='chevrons'>
       </button>
-      <button class='alt' @click='onExportClick(goodProfit)' :disabled='exportMiningRewards?.length === 0'>
+      <button class='alt' @click='onExportClick()' :disabled='exportMiningRewards?.length === 0'>
         {{ $t('MSG_EXPORT_DAILY_OUTPUT_CSV') }}
       </button>
       <button
@@ -94,7 +94,7 @@
 
 <script setup lang='ts'>
 import saveAs from 'file-saver'
-import { useAdminAppGoodStore, useFrontendDetailStore, formatTime, AppGood } from 'npool-cli-v4'
+import { useAdminAppGoodStore, useFrontendDetailStore, formatTime, MiningReward, AppGood } from 'npool-cli-v4'
 import { MyGoodProfit } from 'src/localstore/ledger/types'
 import { defineProps, toRef, computed, ref } from 'vue'
 import { useRouter } from 'vue-router'
@@ -116,50 +116,15 @@ const short = ref(true)
 
 const good = useAdminAppGoodStore()
 const target = computed(() => good.getGoodByID(goodProfit.value?.GoodID))
+const coinUnit = computed(() => good.getGoodByID(goodProfit.value?.GoodID)?.CoinUnit as string)
+const techServiceFee = computed(() => Number(target?.value?.TechnicalFeeRatio) / 100)
+const deservedRatio = computed(() => 1 - techServiceFee.value)
+
+const getStatus = computed(() => (_good: AppGood) => !_good.EnableProductPage || !good.haveSale(_good) || !good.haveStock(_good))
 
 const detail = useFrontendDetailStore()
 const miningDetails = computed(() => detail.MiningRewards.MiningRewards.filter((el) => el.GoodID === goodProfit?.value?.GoodID))
 
-const getStatus = computed(() => (_good: AppGood) => !_good.EnableProductPage || !good.haveSale(_good) || !good.haveStock(_good))
-
-interface ExportMiningReward {
-  CreatedAt: string;
-  Units: string;
-  RewardAmount: string;
-  RewardAmountPerUnit: string;
-  ProverIncentive: number;
-}
-
-const exportMiningRewards = computed(() => Array.from(miningDetails.value).map((el) => {
-  return {
-    CreatedAt: new Date(el.CreatedAt * 1000).toISOString()?.replace('T', ' ')?.replace('.000Z', ' UTC'),
-    Units: `${parseFloat(el.Units)}` + t(goodProfit.value?.GoodUnit),
-    RewardAmount: el.RewardAmount,
-    RewardAmountPerUnit: el.RewardAmountPerUnit,
-    ProverIncentive: Number(good.getGoodByID(el.GoodID)?.DailyRewardAmount) * Number(el.Units)
-  } as ExportMiningReward
-}))
-
-const onExportClick = (row: MyGoodProfit) => {
-  const columns = {
-    CreatedAt: 'Date',
-    Units: 'Mining Unit Amount',
-    RewardAmount: 'Mining Rewards',
-    RewardAmountPerUnit: 'Mining Rewards per 1 Unit'
-  } as Record<string, string>
-  if (row.GoodID === 'de420061-e878-4a8b-986a-805cadd59233') {
-    columns.ProverIncentive = 'Prover Incentive'
-  }
-  const output = stringify(exportMiningRewards.value, {
-    header: true,
-    columns: columns
-  })
-
-  const blob = new Blob([output], { type: 'text/plain;charset=utf-8' })
-  const name = target?.value?.DisplayNames?.[2] ? t(target?.value?.DisplayNames?.[2]) : goodProfit.value?.GoodName
-  const filename = name + '-' + formatTime(new Date().getTime() / 1000) + '.csv'
-  saveAs(blob, filename)
-}
 const router = useRouter()
 const onPurchaseClick = (_good: AppGood) => {
   if (_good.ProductPage?.length === 0 || !_good.ProductPage) {
@@ -171,7 +136,6 @@ const onPurchaseClick = (_good: AppGood) => {
     })
     return
   }
-
   void router.push({
     path: _good?.ProductPage,
     query: {
@@ -184,6 +148,74 @@ const onExpandClick = () => {
   short.value = !short.value
 }
 
+interface ExportMiningReward {
+  CreatedAt: string;
+  Units: string;
+  RewardAmount: string;
+  TechServiceFee: number;
+  NetRewards: number;
+  RewardAmountPerUnit: number;
+  CumulativeTotal: number;
+}
+
+const exportMiningRewards = computed(() => {
+  const rowMap = new Map<string, Array<MiningReward>>()
+  const keys = [] as Array<string>
+
+  miningDetails.value.forEach((el) => {
+    const benefitDate = new Date(el.CreatedAt * 1000).toISOString()?.replace('T', ' ')?.replace('.000Z', ' UTC')?.split(' ')[0]
+    if (!rowMap.get(benefitDate)) {
+      keys.push(benefitDate)
+      rowMap.set(benefitDate, [el])
+    } else {
+      rowMap.get(benefitDate)?.push(el)
+    }
+  })
+
+  const rows = [] as Array<ExportMiningReward>
+  let cumulativeTotal = 0
+
+  keys.sort().forEach((key) => {
+    let units = 0
+    let netRewardAmount = 0
+    rowMap.get(key)?.forEach((el) => {
+      units += Number(el.Units)
+      netRewardAmount += Number(el.RewardAmount)
+      cumulativeTotal += Number(el.RewardAmount) / deservedRatio.value
+    })
+    rows.push({
+      CreatedAt: new Date(Number(rowMap.get(key)?.[0]?.CreatedAt) * 1000).toISOString()?.replace('T', ' ')?.replace('.000Z', ' UTC'),
+      Units: `${units}`,
+      RewardAmount: `${netRewardAmount / deservedRatio.value}`,
+      TechServiceFee: (netRewardAmount / deservedRatio.value) * techServiceFee.value,
+      NetRewards: netRewardAmount,
+      RewardAmountPerUnit: netRewardAmount / units,
+      CumulativeTotal: cumulativeTotal
+    })
+  })
+  return rows
+})
+
+const onExportClick = () => {
+  const columns = {
+    CreatedAt: `${t('MSG_DATE_IN_TEMPLATE')}`,
+    Units: `${t('MSG_UNITS_IN_TEMPLATE')}`,
+    RewardAmount: `${t('MSG_REWARDS_IN_TEMPLATE')} (${coinUnit.value})`,
+    TechServiceFee: `${t('MSG_TECH_SERVICE_FEE_IN_TEMPLATE')} (${coinUnit.value})`,
+    NetRewards: `${t('NSG_NET_REWARDS_IN_TEMPLATE')} (${coinUnit.value})`,
+    RewardAmountPerUnit: `${t('MSG_REWARDS_AMOUNT_PER_UNIT_IN_TEMPLATE')} (${coinUnit.value})`,
+    CumulativeTotal: `${t('MSG_CUMULATIVE_TOTAL_IN_TEMPLATE')} (${coinUnit.value})`
+  } as Record<string, string>
+  const output = stringify(exportMiningRewards.value, {
+    header: true,
+    columns: columns
+  })
+
+  const blob = new Blob([output], { type: 'text/plain;charset=utf-8' })
+  const name = target?.value?.DisplayNames?.[2] ? t(target?.value?.DisplayNames?.[2]) : goodProfit.value?.GoodName
+  const filename = name + '-' + formatTime(new Date().getTime() / 1000) + '.csv'
+  saveAs(blob, filename)
+}
 </script>
 <style lang='sass' scoped>
   .top-line-item .value
