95e231cc958a4af334491af524f12e32ea02bb49
lidaqiang <dqli161@163.com>
2023-03-09 11:54:36 +0800

    update

--- src/teststore/mock/appdefaultgood/const.ts
+++
@@ -1,5 +0,0 @@
-export enum API {
-  GET_APP_DEFAULT_GOODS = '/good/v1/get/appdefaultgoods',
-  CREATE_APP_DEFAULT_GOOD = '/good/v1/create/appdefaultgood',
-  DELETE_APP_DEFAULT_GOOD = '/good/v1/delete/appdefaultgood',
-}
