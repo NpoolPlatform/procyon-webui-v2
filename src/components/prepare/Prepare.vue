8ca320ba34d19f759e2d3873d94aa18c849500bd
lidaqiang <dqli161@163.com>
2023-03-29 16:14:35 +0800

    change router

--- src/components/prepare/Prepare.vue
+++ src/components/prepare/Prepare.vue
@@ -1,21 +1,40 @@
 <script setup lang='ts'>
 import { AppID } from 'src/const/const'
-import { onMounted } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { AppGood, NotifyType, useAdminAppGoodStore, useFrontendAppStore } from 'npool-cli-v4'
 import { useLocalAppStore } from 'src/localstore/app'
+import routes from 'src/router/routes'
+import { RouteRecordRaw } from 'vue-router'
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
 
 const good = useAdminAppGoodStore()
 const application = useFrontendAppStore()
+const newRoutes = ref([] as Array<RouteRecordRaw>)
+
+watch(() => app.App?.Maintaining, () => {
+  // TODO
+})
 
 onMounted(() => {
   if (good.AppGoods.AppGoods.length === 0) {
     getAppGoods(0, 500)
   }
   getApplication()
+
+  if (!app.App?.Maintaining) {
+    routes.forEach((el) => {
+      if (el?.name === 'index') {
+        el.path = '/testing'
+      }
+      if (el?.name === 'maintenance') {
+        el.path = '/'
+      }
+      newRoutes.value.push(el)
+    })
+  }
 })
 
 const getAppGoods = (offset: number, limit: number) => {
