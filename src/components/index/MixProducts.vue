3b1b2667d164965cb5d25a7c71802757dab3db96
lidaqiang <dqli161@163.com>
2023-04-17 10:43:48 +0800

    change aleo & ido

--- src/components/index/MixProducts.vue
+++ src/components/index/MixProducts.vue
@@ -1,33 +1,36 @@
 <template>
   <div class='content'>
     <div class='columns-2 highlight'>
-      <div class='column-2 order-1 with-frame'>
+      <div class='column-2'>
         <h1> <span v-html='$t("MSG_FIRST_BATCH_MINING_TITLE")' /> </h1>
-        <h2>
-          <span v-html='$t("MSG_FIRST_BATCH_MINING_SUBTITLE")' />
-        </h2>
+        <h2><span v-html='$t("MSG_FIRST_BATCH_MINING_SUBTITLE")' /></h2>
         <p v-html='$t("MSG_FIRST_BATCH_MINING_CONTENT")' />
-        <button @click='onAleoClick'>
+        <button @click='onIronFishClick'>
           {{ $t('MSG_LEARN_MORE') }}
         </button>
       </div>
-      <div class='column-2 order-2'>
+      <div class='column-2'>
         <img src='product/aleo/procyon-product-aleo-homepage.png'>
+        <div class='highlight-image'>
+          <img src='project-iron-fish/procyon-product-iron-fish-homepage.jpg'>
+          <span class='caption'>Iron Fish First Batch Mining sold out in less than 24 hours!</span>
+        </div>
       </div>
-      <div class='column-2 order-4'>
-        <img src='product/mixmob/procyon-ido-mixmob-homepage.png'>
-      </div>
-      <div class='column-2 order-3 with-frame'>
-        <h1>
-          <span v-html='$t("MSG_EXCLUSIVE_IDO_TITLE")' />
-        </h1>
-        <h2>
-          <span v-html='$t("MSG_EXCLUSIVE_IDO_SUBTITLE")' />
-        </h2>
+    </div>
+    <div class='columns-2 highlight'>
+      <div class='column-2'>
+        <h1><span v-html='$t("MSG_EXCLUSIVE_IDO_TITLE")' /></h1>
+        <h2><span v-html='$t("MSG_EXCLUSIVE_IDO_SUBTITLE")' /></h2>
         <p v-html='$t("MSG_EXCLUSIVE_IDO_CONTENT")' />
         <button class='in-active' disabled>
           {{ $t('MSG_COMING_SOON') }}
         </button>
+      </div>
+      <div class='column-2'>
+        <div class='highlight-image'>
+          <img src='product/mixmob/procyon-ido-mixmob-homepage.png'>
+          <span class='caption'>{{ $t('MSG_EXCLUSIVE_IDOS_INBOUND') }}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -38,8 +41,8 @@ import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const onAleoClick = () => {
-  void router.push({ path: '/product/aleo' })
+const onIronFishClick = () => {
+  void router.push({ path: '/product/ironfish' })
 }
 
 </script>
