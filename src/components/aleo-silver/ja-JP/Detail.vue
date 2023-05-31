0af0123ef2913ee74e42c47a35e8683c24a9af0d
daqiangli <dqli161@163.com>
2023-05-31 11:31:16 +0800

    add aleo silver page

---
+++ src/components/aleo-silver/ja-JP/Detail.vue
@@ -0,0 +1,350 @@
+<template>
+  <div class='product-detail-text'>
+    <h3>Aleo製品一覧</h3>
+
+    <div class='info-table-container'>
+      <h4 class='info-table-title'>
+        テストネット報酬の内訳:
+      </h4>
+      <table class='info-table table-2-columns'>
+        <thead>
+          <tr>
+            <th><span class='label-platinum'>Aleoプラチナ</span></th>
+            <th><span class='label-gold'>Aleoゴールド</span></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>ファーストバッチマイニング</td>
+            <td>ファーストバッチマイニング</td>
+          </tr>
+          <tr>
+            <td v-html='$t("MSG_ALEO_PURCHASE_DURATION")' />
+            <td v-html='$t("MSG_ALEO_GOLD_PURCHASE_DURATION")' />
+          </tr>
+          <tr>
+            <td>プルーバーテストネット報酬をもらえる<br>*750万クレジット(全体の30％)</td>
+            <td>プルーバーテストネット報酬は<strong>もらえない</strong></td>
+          </tr>
+          <tr>
+            <td>ベリファイアーテストネット報酬ももらえる可能性がある<br>*1750万クレジット(全体の70％の予測)</td>
+            <td><strong>ベリファイアーテストネット報酬はもらえる可能性がある<br>*1750万クレジット（全体の70％の予測）</strong></td>
+          </tr>
+          <tr>
+            <td>1口あたりのハッシュパワー：<strong><br>プルーフ/秒M</strong></td>
+            <td>未定</td>
+          </tr>
+          <tr>
+            <td v-html='$t("MSG_ALEO_TIME_PERIOD")' />
+            <td v-html='$t("MSG_ALEO_GOLD_TIME_PERIOD")' />
+          </tr>
+          <tr>
+            <td>1口あたりのAleoクレジット獲得枚数:<br><strong>まだ計測中、最終的な獲得クレジット数は、アカウントにてご確認ください。</strong></td>
+            <td>未定</td>
+          </tr>
+        </tbody>
+      </table>
+      <p class='info-table-note'>
+        ※<a @click='scrollTo("Aleo Prover Incentive Program")'>Aleoプルーバーインセンティブプログラム</a>の詳細を閲覧
+      </p>
+    </div>
+
+    <h3>Aleoゴールド: メインネットのローンチ前にAleoトークンを獲得しよう!</h3>
+
+    <p>Aleoのプルーバーテストネット報酬プログラムは終了しましたが、プロシオンユーザーは今度はAleoゴールドを購入することができます。</p>
+
+    <p>Aleoは、<strong>まだ1750万テストネット報酬を配布する必要があります</strong>。次の「ベリファイヤー」テストネット報酬プログラムの詳細の発表を楽しみにお待ちください。</p>
+
+    <p>プロシオンのAleoプラチナは、早期購入者への特典を提供しました。</p>
+
+    <p><strong>まだこのファーストバッチマイニングを購入していない方は、次のチャンスをお見逃しなく!</strong></p>
+
+    <p>これらの「ベリファイヤー」テストネット報酬プログラムは、メインネットのAleoトークンへの部分的に交換可能となり、私たちのパートナーHpoolは、すべてのAleo購入者のためにテストネット報酬プログラムに参加し続けています。</p>
+
+    <h3>プロシオンのプロジェクト選定基準</h3>
+
+    <p>プロシオンは、様々な基準からプロジェクトを厳選していますが、その中でも特に重要なものをご紹介いたします:</p>
+
+    <div class='info-table-container table-2-columns'>
+      <table class='info-table'>
+        <thead>
+          <tr>
+            <th id='aqua'>
+              <span>優秀なプロジェクト</span>
+            </th>
+            <th id='pink'>
+              <span>劣っているプロジェクト</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>革新的なテクノロジーを生み出している</td>
+            <td>業界において価値をもたらさない</td>
+          </tr>
+          <tr>
+            <td>コードと進捗を公開している</td>
+            <td>コードを公開せず、進捗の発信が少ない</td>
+          </tr>
+          <tr>
+            <td>著名な大手企業／大手機関投資家から出資を受けている</td>
+            <td>ICOを行い、取引所で上場する前にトークンを直接個人投資家に販売している</td>
+          </tr>
+          <tr>
+            <td>技術開発に時間を費やしている</td>
+            <td>ローンチが早く、質の管理ができていない</td>
+          </tr>
+          <tr>
+            <td>トークンの配布と今後のスケジュールを明確に発表している</td>
+            <td>元本保証を約束し、ポンジスキームのように見える</td>
+          </tr>
+          <tr>
+            <td>活発なコミュニティを構築している</td>
+            <td>ボットやサクラを使ってコミュニティを形成している</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <h3>ファーストバッチマイニングとその優位性</h3>
+
+    <p>プロシオンは、優れたプロジェクトを選別するだけでなく、ユーザー様にとって最高の価値を生み出すために、先行者利益に特化した機会をご提供しております。そのため、最もベストなタイミングのマイニングである、ファーストバッチマイニングをご紹介させていただいております:</p>
+
+    <ul>
+      <li>タイミングが重要: マイニングはメインネット立ち上げ（マイニング初日）が最も収益性が高いです。</li>
+      <li>なぜ初日の収益性が一番高いのか？参加するマイナーの数が少ないので、必然的に1人当たりの報酬が多くなります。</li>
+      <li>マイニングプロジェクトでは、最初の3カ月で参加者が急増するため、早めにスタートがすることが重要です。</li>
+    </ul>
+
+    <h3>なぜAleoがWeb3において重要なのか?</h3>
+
+    <p><img class='project-promo-content' src='product/aleo/aleo-web3-jp.png'></p>
+
+    <ul>
+      <li>Web1 では、情報を検索・交換できるようになりました。Googleなどの検索エンジンが覇権を握りました。</li>
+      <li>Web2 はFacebookや、Twitterなどのプラットフォームが主流となりました、しかし、価値や個人情報が、利便性と引き換えにプラットフォームに吸い取られてしまいました。</li>
+      <li>Web3 のテーマは所有権であり、ユーザー自身が価値や個人情報を所有しているのです。Web3は、インターネットの次の進化形です。</li>
+    </ul>
+
+    <p>この業界を作り上げたパイオニアたちは、中央集権的な銀行が失敗してきた大きな問題を解決してきました。しかし、多くの時間が経過し、次に解決しなければならない大きな問題はプライバシーです。</p>
+
+    <p>プライバシーが低ければ低いほど、ハッカーなどの悪い意図を持った人間たちに対して無防備になるのです。</p>
+
+    <p>自分の家の住所や銀行口座にいくら資産があるかを他人に知られたくないのは当然のことです。</p>
+
+    <p>Aleoはプライバシーにまつわるさまざまな問題の解決の先陣を切ってくれるでしょう。</p>
+
+    <h3>なぜAleoは特別なのか？</h3>
+
+    <h4>Aleoは、史上初の、完全に分散型のプライベートアプリケーションのためのレイヤー1ブロックチェーンです。</h4>
+
+    <p><img class='project-promo-content' src='product/aleo/aleo-privacy-prog-jp.png'></p>
+
+    <p>ほとんどのブロックチェーンは、プライバシーを犠牲にしてプログラミング性を優先しているか、その逆になっています。今のところ、両方の利点をもつプロジェクトはありません。</p>
+
+    <p>Aleoは、Web3をスケールアップし、プログラミング可能で、かつプライバシーが守られた未来を切り開く鍵となるテクノロジーです。</p>
+
+    <p>ゼロ知識証明を使って、Aleoは決済、NFT、dApps、ゲームなどを匿名化することができます。ゼロ知識証明は、インターネットの次の革命となり、オンラインをフル活用する利便性と、プライバシーの保護を両立します。</p>
+
+    <h3>Aleoに出資をしている世界トップクラスのVC一覧</h3>
+
+    <p>Aleoの資金調達額ー255億円 (約2.28億ドル)は、ゼロ知識証明技術において過去最大の資金調達額となります。<a href='https://www.crunchbase.com/search/principal.investors/field/organizations/num_investors/aleo-673f' target='_blank'>ソフトバンク、Coinbase、a16z、その他19社以上の世界トップクラスのVCがAleoに出資をしております。</a></p>
+
+    <div class='logo-display'>
+      <div><img src='vc-logos/softbank-vision-fund.png'></div>
+      <div><img src='vc-logos/a16z.png'></div>
+      <div><img src='vc-logos/polychain-capital.png'></div>
+      <div><img src='vc-logos/coinbase.png'></div>
+      <div><img src='vc-logos/samsung-next.png'></div>
+      <div><img src='vc-logos/slow-ventures.png'></div>
+      <div><img src='vc-logos/dekrypt-capital.png'></div>
+      <div><img src='vc-logos/kora.png'></div>
+      <div><img src='vc-logos/sea-capital.png'></div>
+      <div><img src='vc-logos/tiger-global.png'></div>
+      <div><img src='vc-logos/zkv.png'></div>
+      <div><img src='vc-logos/placeholder.png'></div>
+      <div><img src='vc-logos/scalar-capital.png'></div>
+      <div><img src='vc-logos/galaxy-digital.png'></div>
+      <div><img src='vc-logos/variant.png'></div>
+      <div><img src='vc-logos/ethereal-ventures.png'></div>
+      <div><img src='vc-logos/a_capital.png'></div>
+      <div><img src='vc-logos/david-namdar.png'></div>
+      <div><img src='vc-logos/balaji-srinivasan.png'></div>
+    </div>
+
+    <p>上記のVCは、Aleoが史上初の匿名性のWeb3体験という使命を果たすことに大きな期待を寄せています。イーサリアムベースのプロジェクトの中には、イーサリアムのスケーラビリティ問題を解決するためにゼロ知識証明（ZKP）を使用しているものがあります。</p>
+
+    <h3>Aleoを牽引する優秀なコアチーム</h3>
+
+    <p>Aleoは、非常に優秀なチームによって開発されています。2019年にHoward Wu、Michael Beller、Collin Chin、Raymond Chuによって設立されました。Aleoのチームは、Google、Amazon、Facebookなどの世界トップのIT企業出身者、UC バークレー、ジョンズ・ホプキンズ大学、ニューヨーク大学、コーネル大学などのアメリカを代表する研究に長けた大学卒業者、世界トップクラスの頭脳を持つ暗号技術者およびエンジニア、デザイナー、オペレーターが集結しています。</p>
+
+    <div class='project-team'>
+      <div class='team-member'>
+        <h4 class='name'>
+          Alex Pruden
+        </h4>
+        <h5 class='title'>
+          CEO
+        </h5>
+        <p class='bio'>
+          元米国陸軍歩兵・特殊作戦将校、スタンフォード大学MBAプログラム卒業。a16z、Coinbase、GGV Capitalにて勤務。
+        </p>
+      </div>
+
+      <div class='team-member'>
+        <h4 class='name'>
+          Howard Wu
+        </h4>
+        <h5 class='title'>
+          共同創業者
+        </h5>
+        <p class='bio'>
+          UCバークレー校を卒業後、Googleにエンジニアとして入社。その後バークレー校のブロックチェーンアクセラレーターとして務め、Dekrypt Capitalでは10年以上ブロックチェーンに携わる。
+        </p>
+      </div>
+
+      <div class='team-member'>
+        <h4 class='name'>
+          Collin Chin
+        </h4>
+        <h5 class='title'>
+          共同創業者
+        </h5>
+        <p class='bio'>
+          UCバークレー校在学中にConsenSysでスマートコントラクト開発者としてインターンを経験。Blockchain at BerkeleyとArgusでは、テクニカルアドバイザー兼セキュリティコンサルタントを務める。
+        </p>
+      </div>
+
+      <div class='team-member'>
+        <h4 class='name'>
+          Raymond Chu
+        </h4>
+        <h5 class='title'>
+          共同創業者
+        </h5>
+        <p class='bio'>
+          UCバークレー校在学中、IBMとAmazonでエンジニアとしてインターンを経験。その後、Blockchain at BerkeleyとArgusでプロジェクト・マネージャー兼セキュリティ・コンサルタントを務める。
+        </p>
+      </div>
+
+      <div class='team-member'>
+        <h4 class='name'>
+          Michael Beller
+        </h4>
+        <h5 class='title'>
+          CFO
+        </h5>
+        <p class='bio'>
+          アクセンチュアで7年間シニアマネージャーを務める。数々のIT/金融系スタートアップでCOO/CIOを歴任。
+        </p>
+      </div>
+    </div>
+
+    <h3>Aleoロードマップ</h3>
+
+    <p><img class='project-promo-content' src='product/aleo/aleo-roadmap-jp.png'></p>
+
+    <h3>メディア掲載実績</h3>
+
+    <h4>全世界のトップメディアにAleoが掲載されています！</h4>
+
+    <p><a href='https://www.bloomberg.com/press-releases/2022-03-08/aleo-and-forte-partner-to-power-the-next-generation-of-gaming-economics' target='_blank'>Bloombergで</a>、AleoがFarmVilleシリーズなどのモバイルゲーム最大手のZyngaで有名なThe Forte Platformと提携し、ブロックチェーンベースのデジタル財産権であるNFTやゲームトークンをゲームに導入することを発表したことが紹介されています。フォルテは、Aleoのプライバシー技術を活用し、フォグ・オブ・ウォー機能を持つMMORPG（大人数でのオンラインゲーム）を構築する予定です。NFTとアプリケーション、メインネットローンチの後に採用を開始する予定です。</p>
+
+    <p><a href='https://www.nasdaq.com/articles/zero-knowledge-cryptography-in-2023%3A-the-year-privacy-becomes-practical' target='_blank'>Nasdaqで紹介された</a>、<a href='https://www.coindesk.com/consensus-magazine/2022/12/29/zero-knowledge-cryptography-in-2023-the-year-privacy-becomes-practical/' target='_blank'>AleoのCEO Alex PrudenによるCoinDeskの記事</a>では、この重要なテクノロジーがもたらす4つの影響について述べられています:</p>
+
+    <ul>
+      <li>ZK（ゼロ知識証明）を搭載したレイヤー1のスマートコントラクトブロックチェーンが始動する</li>
+      <li>ロールアップは中央集権管理されたままで、zkEVMも非実用的なままになる。</li>
+      <li>ハードウェアアクセラレーションにより、ZK（ゼロ知識証明）はより実用的で便利なものとなり、あらゆる場面で利用できるようになる。</li>
+      <li>企業はプライバシーについて二者択一を迫られるでしょう。</li>
+    </ul>
+
+    <p><a href='https://www.tomshardware.com/news/new-aleo-crypto-coin-may-give-gpu-miners-a-second-chance' target='_blank'>Tom's Hardwareのレポート</a>によると、多くのETHマイニングプールがAleoテストネットマイニングに移行しているとのことです。</p>
+
+    <h3>取引所への上場</h3>
+
+    <div class='logo-display'>
+      <div><img src='vc-logos/coinbase.png'></div>
+      <div><img src='vc-logos/binance.png'></div>
+      <div><img src='vc-logos/kraken.png'></div>
+      <div />
+      <div><img src='vc-logos/okex.png'></div>
+      <div><img src='vc-logos/huobi.png'></div>
+    </div>
+
+    <p>Aleoに明確にチャンスと注目が集まっています。アーリーアダプターは誰でも、プロジェクトがどの世界トップクラスの取引所に上場するのかが気になるでしょう。そこで、CEX（中央集権取引所）の主な上場基準をご紹介させていただきます。</p>
+
+    <div class='info-table-container'>
+      <table class='info-table'>
+        <thead>
+          <tr>
+            <th />
+            <th>取引所の上場条件</th>
+            <th class='table-highlight'>
+              Aleoが世界トップクラスの取引所に上場するであろう根拠
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><strong>革新的なテクノロジー</strong></td>
+            <td>取引所は、プロジェクトがWeb3空間にどれだけの技術的価値をもたらすか、また将来の潜在的な市場規模を確認するために、競合プロジェクトとテクノロジーの優劣を比較します。</td>
+            <td class='table-highlight'>
+              レイヤー１であるAleoは、そのプライバシー機能により、dApps、DeFi、NFTなどのプロジェクトを匿名化させ、そのテクノロジーで業界に革命を起こすことができます。
+            </td>
+          </tr>
+          <tr>
+            <td><strong>コミュニティの規模感</strong></td>
+            <td>取引所は、より多くの新規顧客を確保するために、プロジェクトが大規模で活発なコミュニティを持っているかどうかを精査します。</td>
+            <td class='table-highlight'>
+              その技術とチームの信頼性により、Aleoはすでに7万人のTwitterフォロワーを獲得し、他のソーシャルメディアチャンネル（Telegram、Discord、AMAなど）でも活発なコミュニティを有しています。
+            </td>
+          </tr>
+          <tr>
+            <td><strong>出資者(VC)</strong></td>
+            <td>取引所は上場前にプロジェクトへの出資者/資本政策表を確認します。大手VCは、プロジェクトの成長を助けるリソースをもたらすためです。</td>
+            <td class='table-highlight'>
+              Aleoは非常に潤沢な資金を有しています。。最初の資金調達ラウンドでは、a16z、Galaxy Digital、Coinbase Ventures、Polychain Capitalが参加、2回目のラウンドでは、SoftBank Vision Fund 2、Tiger Global、Samsung Venturesが参加しました。
+            </td>
+          </tr>
+          <tr>
+            <td><strong>ネットワークの活発度</strong></td>
+            <td>取引所は、プロジェクトのネットワークがどれだけ活発であるかをチェックします。バリデータ／マイナーが多ければ多いほど、ネットワークはより安定し、取引量も多くなります。</td>
+            <td class='table-highlight'>
+              Aleoは現在、テストネット３に入り、メインネットが間近に迫っています。Hpool、f2pool、ZK.Workなどの業界大手マイニングプールがすでにAleoのマイニングに参加しています。
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <h3 id='Aleo Prover Incentive Program'>
+      テストネット報酬インセンティブプログラムが開始！
+    </h3>
+
+    <p>2022年12月2日より、Aleoはテストネット3参加者を対象に<a href='https://www.aleo.org/post/testnet-3-incentives-kickoff' target='_blank'>「テストネット報酬インセンティブプログラム」</a>を開始しました。</p>
+
+    <p>Aleoのファーストバッチマイニング参加者の方は、テストネットの時点で、すでにリターンを獲得しているのです！</p>
+
+    <h4>Aleoテストネット報酬インセンティブプログラムの仕組み:</h4>
+
+    <ul>
+      <li>Aleoは、テストネット3のインセンティブとして2500万Aleoテストネット報酬を確保しています。Aleoテストネット報酬インセンティブプログラムの最初の部分は、「プルーバー」に焦点が当てられます。 「プルーバーインセンティブプログラム（テストネット報酬インセンティブプログラム）」は、750万テストネット報酬（2500万Aleoテストネット報酬全体の30%）を占めます。</li>
+      <li>メインネットローンチ時、Aleoテストネット報酬は5:1（20%）の割合でAleoトークンに交換可能です。ファーストバッチマイニング参加者のみが対象のボーナスです。</li>
+    </ul>
+
+    <p><strong>例：1ユニットがテストネットで100Aleoマイニングした場合、メインネットローンチ時に20Aleoトークンを受け取れる。</strong></p>
+
+    <p>Aleoのテストネット報酬インセンティブプログラムは、メインネットの立ち上げに向けた重要なステップとなります。</p>
+
+    <h3>リスク警告と免責事項：暗号資産は常にリスクを伴います。</h3>
+
+    <p>前提として、プロシオンとそのチームは、ユーザー様のために最善の努力を尽くします。</p>
+
+    <p>とはいえ、一般的なルールとして、ユーザー様はご自分がよく知り、そのリスクを理解しているプロダクトを選択すべきです。これらのプロダクトは、政府の政策、経済状況、市場動向の変化など様々な要因により価値が変動する可能性があります。また、当該プロダクトは、関連する法域の適用法令の対象となります。最後に、オンラインプラットフォーム上で取引を行う場合、当社のコントロール外の状況により、ハードウェア及びソフトウェアの予測不可能な故障を含むシステムに関連するリスクにさらされる可能性があります。</p>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { scrollTo } from 'src/utils/scroll'
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -501,6 +501,19 @@ const routes: RouteRecordRaw[] = [
           ShowSideMenu: false
         }
       }, {
+        path: 'product/aleosilver',
+        component: () => import('pages/product/AleoSilver.vue'),
+        meta: {
+          ShowHeaderAnnouncement: true,
+          ShowMainHeader: true,
+          ShowBigLogo: true,
+          ShowFooterTop: true,
+          ShowSignHelper: true,
+          ShowTopTip: false,
+          NeedLogined: false,
+          ShowSideMenu: false
+        }
+      }, {
         path: 'product/ironfish',
         component: () => import('pages/product/IronFish2.vue'),
         meta: {
