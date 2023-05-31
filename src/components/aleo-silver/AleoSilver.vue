46782a8c1cf136752eff2c9dc3a48229babd6142
daqiangli <dqli161@163.com>
2023-05-31 11:55:56 +0800

    add new product page

---
+++ src/components/aleo-silver/AleoSilver.vue
@@ -0,0 +1,223 @@
+<template>
+  <ProductPage
+    :good-id='goodID'
+    :purchase-amount='purchaseAmount'
+    project-class='project-aleo'
+    bg-img='product/aleo/aleo-banner.jpg'
+    :customize-info='true'
+  >
+    <template #product-info>
+      <div class='three-section'>
+        <h4>{{ $t('MSG_PRICE') }}:</h4>
+        <span class='number'>{{ util.getLocaleString(good.getPrice(goodID)) }}</span>
+        <span class='unit'>{{ PriceCoinName }}</span>
+        <div class='tooltip'>
+          <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_PRICE_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='three-section'>
+        <h4>{{ $t('MSG_DAILY_MINING_REWARDS') }}:</h4>
+        <span class='number'>*</span>
+        <span class='unit'>{{ target?.CoinUnit }} / {{ $t('MSG_DAY') }}</span>
+        <div class='tooltip'>
+          <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_DAILY_REWARD_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='three-section'>
+        <h4>{{ $t('MSG_SERVICE_PERIOD') }}:</h4>
+        <span class='number'>{{ util.getLocaleString(target?.DurationDays) }}</span>
+        <span class='unit'>{{ $t('MSG_DAYS') }}</span>
+        <div class='tooltip'>
+          <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_SERVICE_PERIOD_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='three-section'>
+        <h4>{{ $t('MSG_TECHNIQUE_SERVICE_FEE') }}:</h4>
+        <span class='number'>20</span>
+        <span class='unit'>%</span>
+        <div class='tooltip'>
+          <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_TECHNIQUE_SERVICE_FEE_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='three-section'>
+        <h4>{{ $t('MSG_ORDER_EFFECTIVE') }}:</h4>
+        <!-- <span class='number'>{{ true ? 'TBD*' : good.getFormatTime(target?.StartAt) }}</span> -->
+        <span class='number'>{{ $t("MSG_WAIT_CONFIRM") }}</span>
+        <div class='tooltip'>
+          <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_ORDER_EFFECTIVE_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='three-section'>
+        <h4>{{ $t("MSG_SALE_END_DATE") }}</h4>
+        <span class='number'>{{ $t("MSG_WAIT_CONFIRM") }}</span>
+        <br>
+        <div class='tooltip'>
+          <img class='more-info' src='font-awesome/question.svg'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_SALE_END_DATE_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='hr' />
+    </template>
+    <template #product-detail>
+      <div v-if='locale === "en-US"'>
+        <ProductDetailUS />
+      </div>
+      <div v-else>
+        <ProductDetailJP />
+      </div>
+    </template>
+    <template #sidebar>
+      <div id='project-links' class='product-sidebar-section'>
+        <h3 class='form-title'>
+          {{ $t('MSG_PROJECT_LINKS') }}
+        </h3>
+        <ul class='product-links'>
+          <li>
+            <a href='https://www.aleo.org/' target='_blank'><img class='link-icon' src='font-awesome/homepage.svg'>
+              <span>{{ $t('MSG_ALEO_HOMEPAGE') }}</span>
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com/AleoHQ' target='_blank'><img class='link-icon' src='font-awesome/twitter.svg'>
+              <span>{{ $t('MSG_TWITTER') }}</span>
+            </a>
+          </li>
+          <li>
+            <a href='https://www.youtube.com/channel/UCS_HKT2heOC_q88YQLiJt0g' target='_blank'><img class='link-icon' src='font-awesome/youtube.svg'>
+              <span>{{ $t('MSG_YOUTUBE') }}</span>
+            </a>
+          </li>
+          <li>
+            <a href='https://discord.com/invite/aleohq' target='_blank'><img class='link-icon' src='font-awesome/discord.svg'>
+              <span>{{ $t('MSG_DISCORD') }}</span>
+            </a>
+          </li>
+          <li>
+            <a href='https://github.com/AleoHQ' target='_blank'><img class='link-icon' src='font-awesome/github.svg'>
+              <span>{{ $t('MSG_GITHUB') }}</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </template>
+  </ProductPage>
+</template>
+
+<script setup lang='ts'>
+import { defineAsyncComponent, computed, onMounted, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { PriceCoinName } from 'npool-cli-v2'
+import { useI18n } from 'vue-i18n'
+
+import question from '../../assets/question.svg'
+// import lightbulb from '../../assets/lightbulb.svg'
+import { AppGood, InvalidID, NotifyType, useAdminAppCoinStore, useAdminAppGoodStore, useAdminCoinDescriptionStore, useAdminCurrencyStore, useLocaleStringStore } from 'npool-cli-v4'
+import { getCurrencies, getDescriptions } from 'src/api/chain'
+
+// eslint-disable-next-line @typescript-eslint/unbound-method
+const { t, locale } = useI18n({ useScope: 'global' })
+
+const ProductPage = defineAsyncComponent(() => import('src/components/aleo-silver/ProductPage.vue'))
+const ProductDetailUS = defineAsyncComponent(() => import('src/components/aleo-silver/en-US/Detail.vue'))
+const ProductDetailJP = defineAsyncComponent(() => import('src/components/aleo-silver/ja-JP/Detail.vue'))
+
+interface Query {
+  goodId: string;
+  purchaseAmount: number;
+}
+
+const route = useRoute()
+const query = computed(() => route.query as unknown as Query)
+
+const util = useLocaleStringStore()
+
+const coin = useAdminAppCoinStore()
+
+// Use CoinUnit to find GoodID from AppCoin
+const coinUnit = 'ALEO'
+const defaultGoodID = computed(() => {
+  if (coin.AppCoins.AppCoins?.length === 0) {
+    return `${InvalidID}_`
+  }
+  const goodID = coin.getGoodIDByCoinUnit(coinUnit)
+  if (!goodID) {
+    return InvalidID
+  }
+  return goodID
+})
+
+const goodID = computed(() => query.value.goodId?.length ? query.value.goodId : defaultGoodID.value)
+
+const purchaseAmount = computed(() => query.value.purchaseAmount)
+
+const good = useAdminAppGoodStore()
+const target = computed(() => good.getGoodByID(goodID.value) as AppGood)
+
+const currency = useAdminCurrencyStore()
+const description = useAdminCoinDescriptionStore()
+
+const router = useRouter()
+
+watch(defaultGoodID, () => {
+  if (defaultGoodID.value === InvalidID) {
+    void router.push({ path: '/' })
+  }
+})
+
+onMounted(() => {
+  console.log('CoinUnit: ', coinUnit)
+
+  if (description.CoinDescriptions.CoinDescriptions.length === 0) {
+    getDescriptions(0, 100)
+  }
+
+  if (currency.Currencies.Currencies.length === 0 || currency.expired()) {
+    currency.$reset()
+    getCurrencies(0, 10)
+  }
+
+  if (defaultGoodID.value === InvalidID) {
+    void router.push({ path: '/' })
+    return
+  }
+
+  if (defaultGoodID.value === `${InvalidID}_`) {
+    return
+  }
+
+  good.getAppGood({
+    GoodID: goodID.value,
+    Message: {
+      Error: {
+        Title: t('MSG_GET_GOOD'),
+        Message: t('MSG_GET_GOOD_FAIL'),
+        Popup: true,
+        Type: NotifyType.Error
+      }
+    }
+  }, () => {
+    // TODO
+  })
+})
+
+</script>
+
+<style lang='sass' scoped>
+</style>
