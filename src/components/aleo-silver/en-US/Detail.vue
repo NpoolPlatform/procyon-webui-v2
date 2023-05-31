661d929394d32e2abcc422358ee4112bd1c397b0
daqiangli <dqli161@163.com>
2023-05-31 11:57:18 +0800

    add detail page

--- src/components/aleo-silver/en-US/Detail.vue
+++ src/components/aleo-silver/en-US/Detail.vue
@@ -1,65 +1,27 @@
 <template>
   <div class='product-detail-text'>
-    <h3>Aleo Testnet Incentives</h3>
-
-    <div class='info-table-container'>
-      <h4 class='info-table-title'>
-        Incentive Breakdown*
-      </h4>
-      <table class='info-table table-2-columns'>
-        <thead>
-          <tr>
-            <th><span class='label-platinum'>Aleo Platinum</span></th>
-            <th><span class='label-gold'>Aleo Gold</span></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>First Batch Mining</td>
-            <td>First Batch Mining</td>
-          </tr>
-          <tr>
-            <td v-html='$t("MSG_ALEO_PURCHASE_DURATION")' />
-            <td v-html='$t("MSG_ALEO_GOLD_PURCHASE_DURATION")' />
-            <!-- <td>Purchased <strong>before</strong> February 3, 2023</td> -->
-            <!-- <td>Purchased <strong>after</strong> February 3, 2023, <strong>before</strong> March 31st, 2023</td> -->
-          </tr>
-          <tr>
-            <td>You will receive Prover Testnet Rewards<br>*7.5 million credits (30% of total)</td>
-            <td>You will <strong>not receive</strong> Prover testnet rewards</td>
-          </tr>
-          <tr>
-            <td>Verifier Testnet Rewards may ALSO be received.<br>*17.5 million credits (70% of total projected)</td>
-            <td><strong>Verifier Testnet Rewards projected to be<br>*17.5 million credits (70% of total projected)</strong></td>
-          </tr>
-          <tr>
-            <td>Hashpower per unit: <strong>90 proofs / second</strong></td>
-            <td>TBD</td>
-          </tr>
-          <tr>
-            <td v-html='$t("MSG_ALEO_TIME_PERIOD")' />
-            <td v-html='$t("MSG_ALEO_GOLD_TIME_PERIOD")' />
-          </tr>
-          <tr>
-            <td>Aleo credits earned per unit:<br><strong>Still in the process of accounting, please refer to your account for the final number of credits earned</strong></td>
-            <td>TBD</td>
-          </tr>
-        </tbody>
-      </table>
-      <p class='info-table-note'>
-        *See <a @click='scrollTo("Aleo Prover Incentive Program")'>Aleo Prover Incentive Program</a> for a full explanation of the incentives.
-      </p>
+    <div class='warning'>
+      <img src='font-awesome/warning.svg'>
+      <span>A hard sales deadline is not set for Aleo First Batch Mining: Silver. The sale will close shortly before mainnet launch.</span>
     </div>
 
-    <h3>Aleo GOLD - Earn Aleo tokens before the mainnet launches!</h3>
+    <br>
 
-    <p>Aleo’s Prover Incentive program is over, which means Procyon users can now buy Aleo Gold</p>
+    <h3>Why is Aleo Essential to Web3?</h3>
 
-    <p><strong>Aleo still has 17.5 million testnet credits that need to be distributed</strong>. We expect the next incentive program to be announced for testnet verifiers: stay tuned for updates!</p>
+    <p><img class='project-promo-content' src='product/aleo/aleo-web3-en.png'></p>
 
-    <p>Procyon’s Aleo Platinum earned early rewards for early buyers. <strong>If you haven’t yet bought this first-batch mining, don’t miss this next chance!</strong></p>
+    <ul>
+      <li>Web1 was about making information searchable &amp; exchangeable. Google was the big winner.</li>
+      <li>Web2 was about platforms and gave us Facebook, but value only flowed through these massive platforms.</li>
+      <li>Web3 is about ownership: users are capturing value themselves! Web3 is the next evolutionary step of the internet!</li>
+    </ul>
 
-    <p>These testnet incentives are eligible for partial conversion to mainnet Aleo tokens, and our partner Hpool continues to participate for all of our Aleo buyers.</p>
+    <p>Since the creation of blockchain technology and cryptocurrency, privacy has been one of the biggest unresolved issues. This industry has pioneered transparency and decentralization, solving big problems at which centralized banking has so far failed.</p>
+
+    <p>However, a great deal of time has gone by, and the next big problem that needs to be solved is privacy. The less privacy someone has the more vulnerable they are to people with bad agendas. It’s equivalent to "Do you want people to know your home address?" "Do you want people to know how much money you have in your bank account?"</p>
+
+    <p>Projects such as Aleo will spearhead the solution to privacy on blockchain.</p>
 
     <h3>Procyon’s Project Selection</h3>
 
@@ -109,32 +71,6 @@
         </tbody>
       </table>
     </div>
-
-    <h3>First Batch Mining and its Advantages</h3>
-
-    <p>In addition to filtering for great projects, Procyon selects for early opportunities to generate the best value for our users. Therefore, we prioritize first batch mining:</p>
-
-    <ul>
-      <li>Timing is important: mining is most profitable from the first day of mainnet launch.</li>
-      <li>Why is the first day the most profitable? There are fewer miners participating, hence more rewards to be distributed per miner.</li>
-      <li>Mining projects see a rapid increase in the number of miners in the first three months: it is essential to start early.</li>
-    </ul>
-
-    <h3>Why is Aleo Essential to Web3?</h3>
-
-    <p><img class='project-promo-content' src='product/aleo/aleo-web3-en.png'></p>
-
-    <ul>
-      <li>Web1 was about making information searchable &amp; exchangeable. Google was the big winner.</li>
-      <li>Web2 was about platforms and gave us Facebook, but value only flowed through these massive platforms.</li>
-      <li>Web3 is about ownership: users are capturing value themselves! Web3 is the next evolutionary step of the internet!</li>
-    </ul>
-
-    <p>Since the creation of blockchain technology and cryptocurrency, privacy has been one of the biggest unresolved issues. This industry has pioneered transparency and decentralization, solving big problems at which centralized banking has so far failed.</p>
-
-    <p>However, a great deal of time has gone by, and the next big problem that needs to be solved is privacy. The less privacy someone has the more vulnerable they are to people with bad agendas. It’s equivalent to "Do you want people to know your home address?" "Do you want people to know how much money you have in your bank account?"</p>
-
-    <p>Projects such as Aleo will spearhead the solution to privacy on blockchain.</p>
 
     <h3>Why is Aleo Special?</h3>
 
@@ -242,7 +178,7 @@
 
     <h3>Project Roadmap</h3>
 
-    <p><img class='project-promo-content' src='product/aleo/aleo-roadmap-en.png'></p>
+    <p><img class='project-promo-content' src='project-aleo/aleo-roadmap-en.png'></p>
 
     <h3>Media Coverage</h3>
 
@@ -318,34 +254,24 @@
       </table>
     </div>
 
-    <h3 id='Aleo Prover Incentive Program'>
-      Aleo Prover Incentive Program
-    </h3>
+    <h3>First Batch Mining and its Advantages</h3>
 
-    <p>Since December 2nd, 2022, <a href='https://www.aleo.org/post/testnet-3-incentives-kickoff' target='_blank'>Aleo has implemented "Prover Incentives"</a> for its testnet participants. Aleo credits that are mined during this phase of the testnet will become redeemable for Aleo mainnet tokens at a 5:1 ratio (20%) upon mainnet launch. To put it simply, Aleo first-batch mining is already generating tokens even before mainnet launch!</p>
-
-    <h4>Aleo incentives program — how it works:</h4>
+    <p>In addition to filtering for great projects, Procyon selects for early opportunities to generate the best value for our users. Therefore, we prioritize first batch mining:</p>
 
     <ul>
-      <li>Aleo has set aside 25 million Aleo credits for Testnet 3 incentives. The initial part of the Aleo Incentives program will be focused on Provers. Prover Incentives account for <strong>7.5 million credits (30% of 25 million total Aleo credits)</strong>.</li>
-      <li>At mainnet launch, <strong>Aleo credits will be redeemable at a rate of 5:1 (20%)</strong>. A bonus for first-batch miners!</li>
-      <li>Aleo credits to be redeemed for Aleo tokens at a ratio of 5:1 upon mainnet launch.</li>
+      <li>Timing is important: mining is most profitable from the first day of mainnet launch.</li>
+      <li>Why is the first day the most profitable? There are fewer miners participating, hence more rewards to be distributed per miner.</li>
+      <li>Mining projects see a rapid increase in the number of miners in the first three months: it is essential to start early.</li>
     </ul>
-
-    <p>Again, the 5:1 ratio refers to the fact that 20% of Aleo testnet rewards can be redeemed for Aleo tokens upon mainnet launch. Testnet rewards can be earned by participating in the Testnet Prover Incentive Program.</p>
-
-    <p><strong>e.g. if 1 unit mines 100 testnet rewards, they will be eligible to receive 20 Aleo tokens upon mainnet launch.</strong></p>
-
-    <p>The Aleo testnet reward is a significant step towards its mainnet launch.</p>
 
     <h3>Risk warning and disclaimer: cryptocurrencies involve risk.</h3>
 
-    <p>First of all, Procyon and its team will make best efforts for its users.</p>
+    <h4>First of all, Procyon and its team will make best efforts for its users.</h4>
 
     <p>That said, as a general rule, one should only select products that they are familiar with and understand the risk associated with them. Value of such products may fluctuate due to a variety of factors, such as changes in government policies, economic conditions, or market trends. Further, such products are subject to the applicable laws and regulations of the relevant overseas jurisdiction in which these products are held. Lastly, if one undertakes transactions on an online platform, they may be exposed to risks associated with the system including the unpredictable failure of hardware and software due to circumstances outside of our control.</p>
   </div>
 </template>
 
 <script setup lang='ts'>
-import { scrollTo } from 'src/utils/scroll'
+// import { scrollTo } from 'src/utils/scroll'
 </script>
