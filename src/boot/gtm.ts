ed5bece8110202e674894e614bf156a46fc2972c
daqiangli <dqli161@163.com>
2023-06-20 15:26:55 +0800

    update gtm

---
+++ src/boot/gtm.ts
@@ -0,0 +1,15 @@
+import { boot } from 'quasar/wrappers'
+import VueGtag from 'vue-gtag'
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+export default boot(({ app, router }) => {
+  app.use(
+    VueGtag,
+    {
+      config: {
+        id: 'G-WMDW59ENZM'
+      }
+    },
+    router
+  )
+})
