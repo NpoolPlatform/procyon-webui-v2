c7ac528cf9a5c58dc27a77987591e16d135e7b25
daqiangli <dqli161@163.com>
2023-07-19 15:19:09 +0800

    add typesense

--- quasar.conf.js
+++ quasar.conf.js
@@ -43,7 +43,8 @@ module.exports = configure(function (ctx) {
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [
-      'app.sass'
+      'app.sass',
+      'docsearch.sass'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
