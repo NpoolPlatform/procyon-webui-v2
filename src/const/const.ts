80891ae12b0d06c8ec6a7f1119e1dc7c90267b1d
daqiangli <dqli161@163.com>
2023-07-20 18:25:47 +0800

    delete it & add typesenseapikey

--- src/const/const.ts
+++ src/const/const.ts
@@ -2,7 +2,7 @@ const LogoText = 'PROCYON'
 
 const AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
 const ThrottleSeconds = 10
-const TypesenseApiKey = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
+const TypesenseApiKey = 'c5deba40-4214-469e-ba6b-5583e9e53e6b'
 
 enum IntervalKey {
   All = 'All',
