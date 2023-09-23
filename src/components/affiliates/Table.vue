cd7987d5364a1d3bce8fc06faafc83a289853145
Zhao KK <kikakkz@hotmail.com>
2023-09-23 15:20:30 +0800

    Correct superior commission

--- src/components/affiliates/Table.vue
+++ src/components/affiliates/Table.vue
@@ -120,7 +120,7 @@ const goodUnit = computed(() => {
 })
 const totalUnits = computed(() => _achievement.totalInviteeUnits(undefined, logined.loginedUserID, selectedCoinID.value, undefined, false))
 const totalAmount = computed(() => _achievement.totalInviteeAmount(undefined, logined.loginedUserID, selectedCoinID.value, undefined, false))
-const totalSuperiorCommission = computed(() => _achievement.totalSuperiorCommission(undefined, logined.loginedUserID, selectedCoinID.value))
+const totalSuperiorCommission = computed(() => _achievement.totalInviteeSuperiorCommission(undefined, logined.loginedUserID, selectedCoinID.value, undefined, false))
 
 const userTotalUnits = computed(() => (referral: achievement.Achievement) => _achievement.totalUnits(undefined, referral.UserID, selectedCoinID.value))
 const userTotalAmount = computed(() => (referral: achievement.Achievement) => _achievement.totalAmount(undefined, referral.UserID, selectedCoinID.value))
