45ddad5da1eb0935dedce7c038ed56b3750687f9
Zhao KK <kikakkz@hotmail.com>
2023-09-23 21:58:22 +0800

    Optimize lang switcher

--- src/components/lang/LangSwitcher.vue
+++ src/components/lang/LangSwitcher.vue
@@ -2,7 +2,7 @@
   <ul class='language-picker'>
     <li
       v-for='language in langs'
-      :class='[ curLang === language.Lang ? "selected" : "" ]'
+      :class='[ curLang === language.Lang ? "selected" : "", throtting ? "disabled" : "" ]'
       :key='language.ID'
       @click='onLangClick(language)'
     >
@@ -17,6 +17,7 @@
 import { applang, _locale, user, notif, g11nbase } from 'src/npoolstore'
 import { getNotifs } from 'src/api/notif'
 import { computed } from 'vue'
+import { useSettingStore } from 'src/localstore'
 
 const lang = applang.useAppLangStore()
 const langs = computed(() => lang.langs(undefined))
@@ -28,8 +29,11 @@ const logined = user.useLocalUserStore()
 const _user = user.useUserStore()
 
 const _notif = notif.useNotifStore()
+const _setting = useSettingStore()
+const throtting = computed(() => _setting.LangThrottling)
 
 const onLangClick = (language: g11nbase.AppLang) => {
+  _setting.LangThrottling = true
   locale.setLang(language)
   if (logined.logined) {
     _notif.$reset()
@@ -44,3 +48,9 @@ const onLangClick = (language: g11nbase.AppLang) => {
 }
 
 </script>
+
+<style lang='sass' scoped>
+.disabled
+  pointer-events: none
+  color: grey
+</style>
