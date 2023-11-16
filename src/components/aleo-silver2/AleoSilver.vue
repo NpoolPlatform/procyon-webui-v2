70d9c31149cba4f34685ead8104e1c7db4d4f933
lidaqiang <dqli161@163.com>
2023-11-16 10:27:01 +0800

    add new aleo silver product page

---
+++ src/components/aleo-silver2/AleoSilver.vue
@@ -0,0 +1,206 @@
+<template>
+  <ProductPage
+    :app-good-i-d='(appGoodID as string)'
+    :purchase-amount='purchaseAmount'
+    project-class='project-aleo'
+    bg-img='product/aleo/aleo-banner.jpg'
+    :customize-info='true'
+  >
+    <template #product-info>
+      <div class='three-section'>
+        <h4>{{ $t('MSG_PRICE') }}:</h4>
+        <span class='number'>{{ utils.getLocaleString(good.priceString(undefined, appGoodID as string)) }}</span>
+        <span class='unit'>{{ constant.PriceCoinName }} / {{ target?.Unit ? $t(target?.Unit) : '' }}</span>
+        <div class='tooltip'>
+          <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_PRICE_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='three-section'>
+        <h4>{{ $t('MSG_DAILY_MINING_REWARDS') }}:</h4>
+        <span class='number'>*</span>
+        <span class='unit'>{{ target?.CoinUnit }} / {{ $t('MSG_DAY') }}</span>
+        <div class='tooltip'>
+          <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_DAILY_REWARD_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='three-section'>
+        <h4>{{ $t('MSG_SERVICE_PERIOD') }}:</h4>
+        <span class='number'>{{ target?.DurationDays }}</span>
+        <span class='unit'>{{ $t('MSG_DAYS') }}</span>
+        <div class='tooltip'>
+          <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_SERVICE_PERIOD_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='three-section'>
+        <h4>{{ $t('MSG_TECHNIQUE_SERVICE_FEE') }}:</h4>
+        <span class='number'>20</span>
+        <span class='unit'>%</span>
+        <div class='tooltip'>
+          <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_TECHNIQUE_SERVICE_FEE_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='three-section'>
+        <h4>{{ $t('MSG_ORDER_EFFECTIVE') }}:</h4>
+        <span class='number'>{{ target?.ServiceStartAt === 0 ? 'TBD*' : utils.formatTime(target?.ServiceStartAt as number, 'YYYY-MM-DD') }}</span>
+        <br>
+        <span class='unit'>{{ utils.formatTime(target?.ServiceStartAt as number, 'YYYY-MM-DD') }} {{ $t("MSG_JST") }}</span>
+        <div class='tooltip'>
+          <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_ORDER_EFFECTIVE_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='three-section'>
+        <h4>{{ $t("MSG_SALE_END_DATE") }}</h4>
+        <span class='number'>{{ $t("MSG_SALE_END_WAIT_CONFIRM") }}</span>
+        <br>
+        <div class='tooltip'>
+          <img class='more-info' src='font-awesome/question.svg'><span>{{ $t('MSG_LEARN_MORE') }}</span>
+          <p class='tooltip-text'>
+            {{ $t('MSG_SALE_END_DATE_TIP') }}
+          </p>
+        </div>
+      </div>
+      <div class='hr' />
+    </template>
+    <template #product-detail>
+      <div v-if='locale === "en-US"'>
+        <ProductDetailUS />
+      </div>
+      <div v-else>
+        <ProductDetailJP />
+      </div>
+    </template>
+    <template #sidebar>
+      <div id='project-links' class='product-sidebar-section'>
+        <h3 class='form-title'>
+          {{ $t('MSG_PROJECT_LINKS') }}
+        </h3>
+        <ul class='product-links'>
+          <li>
+            <a href='https://www.aleo.org/' target='_blank'><img class='link-icon' src='font-awesome/homepage.svg'>
+              <span>{{ $t('MSG_ALEO_HOMEPAGE') }}</span>
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com/AleoHQ' target='_blank'><img class='link-icon' src='font-awesome/twitter.svg'>
+              <span>{{ $t('MSG_TWITTER') }}</span>
+            </a>
+          </li>
+          <li>
+            <a href='https://www.youtube.com/channel/UCS_HKT2heOC_q88YQLiJt0g' target='_blank'><img class='link-icon' src='font-awesome/youtube.svg'>
+              <span>{{ $t('MSG_YOUTUBE') }}</span>
+            </a>
+          </li>
+          <li>
+            <a href='https://discord.com/invite/aleohq' target='_blank'><img class='link-icon' src='font-awesome/discord.svg'>
+              <span>{{ $t('MSG_DISCORD') }}</span>
+            </a>
+          </li>
+          <li>
+            <a href='https://github.com/AleoHQ' target='_blank'><img class='link-icon' src='font-awesome/github.svg'>
+              <span>{{ $t('MSG_GITHUB') }}</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </template>
+  </ProductPage>
+</template>
+
+<script setup lang='ts'>
+import { defineAsyncComponent, computed, onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+import { getCoins, getCurrencies, getDescriptions } from 'src/api/chain'
+import { appgood, notify, appcoin, appcoindescription, coincurrency, utils, constant } from 'src/npoolstore'
+
+import question from '../../assets/question.svg'
+
+// eslint-disable-next-line @typescript-eslint/unbound-method
+const { locale } = useI18n({ useScope: 'global' })
+
+const ProductPage = defineAsyncComponent(() => import('src/components/aleo-silver2/ProductPage.vue'))
+const ProductDetailUS = defineAsyncComponent(() => import('src/components/aleo-silver2/en-US/Detail.vue'))
+const ProductDetailJP = defineAsyncComponent(() => import('src/components/aleo-silver2/ja-JP/Detail.vue'))
+
+interface Query {
+  appGoodID: string
+  purchaseAmount: number
+}
+
+const route = useRoute()
+const query = computed(() => route.query as unknown as Query)
+
+const getGood = () => {
+  if (_good.value) {
+    return
+  }
+  if (!appGoodID.value) {
+    void router.push({ path: '/' })
+    return
+  }
+  good.getAppGood({
+    ID: appGoodID.value,
+    Message: {
+      Error: {
+        Title: 'MSG_GET_GOOD',
+        Message: 'MSG_GET_GOOD_FAIL',
+        Popup: true,
+        Type: notify.NotifyType.Error
+      }
+    }
+  }, () => {
+    if (!_good.value) {
+      void router.push({ path: '/' })
+    }
+  })
+}
+
+const coin = appcoin.useAppCoinStore()
+// Use CoinUnit to find AppGoodID from AppDefaultGood
+const coinUnit = 'ALEO'
+const purchaseAmount = computed(() => query.value.purchaseAmount)
+
+const good = appgood.useAppGoodStore()
+const appGoodID = computed(() => query.value?.appGoodID || coin.defaultGoodID(undefined, coinUnit))
+const target = computed(() => good.good(undefined, appGoodID.value as string))
+const _good = computed(() => good.good(undefined, appGoodID.value as string))
+
+const currency = coincurrency.useCurrencyStore()
+const description = appcoindescription.useCoinDescriptionStore()
+
+const router = useRouter()
+
+onMounted(() => {
+  if (!coin.coins(undefined).length) {
+    getCoins(0, 100, () => {
+      getGood()
+    })
+  } else {
+    getGood()
+  }
+  if (!description.descriptions(undefined)?.length) {
+    getDescriptions(0, 100)
+  }
+  if (!currency.currencies.length) {
+    getCurrencies(0, 100)
+  }
+})
+</script>
+
+<style lang='sass' scoped>
+</style>
---
+++ src/components/aleo-silver2/ProductPage.vue
@@ -0,0 +1,307 @@
+<template>
+  <div class='content order-page' v-if='showMe'>
+    <div :class='[ "product-container content-glass project", projectClass ]'>
+      <div class='product-title-section project-title-section' :style='{"background-image": "url(" + bgImg + ")"}'>
+        <div class='product-title-container'>
+          <div class='product-page-icon'>
+            <img :src='target?.CoinLogo'>
+          </div>
+          <h1 v-html='good.displayName(undefined, target?.ID as string, 1)' />
+        </div>
+      </div>
+      <!-- mobile start -->
+      <div id='product-form' class='product-sidebar-section mobile'>
+        <h3 class='form-title'>
+          {{ $t('MSG_MINING_PURCHASE') }}
+        </h3>
+        <form action='javascript:void(0)' id='purchase'>
+          <div class='full-section' v-if='good.canBuy(undefined, target?.ID as string)'>
+            <h4>{{ $t("MSG_SALE_END_DATE") }}</h4>
+            <span class='number'>{{ remainDays }}</span>
+            <span class='unit'> {{ $t("MSG_DAYS") }} </span>
+            <span class='number'>{{ remainHours }}</span>
+            <span class='unit'>{{ $t("MSG_HOURS") }} </span>
+            <span class='number'>{{ remainMinutes }}</span>
+
+            <span class='unit'>{{ $t("MSG_MINUTES") }} </span>
+          </div>
+          <h4>{{ $t('MSG_PURCHASE_AMOUNT') }}</h4>
+          <Input
+            v-model:value='myPurchaseAmount'
+            type='number'
+            id='amount'
+            required
+            :error='purchaseAmountError'
+            :message='$t("MSG_AMOUNT_TIP", {MAX: total})'
+            placeholder='MSG_AMOUNT_PLACEHOLDER'
+            :min='1'
+            :max='total'
+            @focus='onPurchaseAmountFocusIn'
+            @blur='onPurchaseAmountFocusOut'
+          />
+          <h4>{{ $t('MSG_PAYMENT_METHOD') }}</h4>
+          <CoinSelector
+            v-model:id='selectedCoinID'
+            :coins='coins'
+            label=''
+            hide-label
+            default
+            :tip-index='1'
+            :name-index='1'
+          />
+          <div class='warning' v-if='showRateTip'>
+            <img :src='warning'>
+            <span>{{ $t('MSG_COIN_USDT_EXCHANGE_RATE_TIP', { COIN_NAME: paymentCoin?.Unit }) }}</span>
+          </div>
+          <div class='warning' v-if='showBUSDTip'>
+            <img :src='warning'>
+            <span>{{ $t('MSG_COIN_BUSD_PAYMENT_TIP') }}</span>
+          </div>
+          <div class='submit-container'>
+            <WaitingBtn
+              label='MSG_PURCHASE'
+              type='submit'
+              class='submit-btn'
+              :disabled='submitting || !target?.EnablePurchase || !good.canBuy(undefined, target?.ID) || good.purchaseLimit(undefined, target?.ID) <= 0'
+              :waiting='submitting'
+              @click='onPurchaseClick'
+            />
+          </div>
+        </form>
+      </div>
+      <div class='info'>
+        <h3 class='form-title'>
+          {{ $t('MSG_PRODUCT_DETAILS') }}
+        </h3>
+        <div v-if='customizeInfo' class='info-flex'>
+          <slot name='product-info' />
+          <div class='product-detail-text'>
+            <slot name='product-detail' />
+          </div>
+        </div>
+      </div>
+      <!-- pc start -->
+      <div class='product-sidebar'>
+        <div id='product-form' class='product-sidebar-section'>
+          <h3 class='form-title'>
+            {{ $t('MSG_MINING_PURCHASE') }}
+          </h3>
+          <form action='javascript:void(0)' id='purchase'>
+            <div class='full-section' v-if='good.canBuy(undefined, target?.ID as string)'>
+              <h4>{{ $t("MSG_SALE_END_DATE") }}</h4>
+              <span class='number'>{{ remainDays }}</span>
+              <span class='unit'> {{ $t("MSG_DAYS") }} </span>
+              <span class='number'>{{ remainHours }}</span>
+              <span class='unit'>{{ $t("MSG_HOURS") }} </span>
+              <span class='number'>{{ remainMinutes }}</span>
+              <span class='unit'>{{ $t("MSG_MINUTES") }} </span>
+            </div>
+            <h4>{{ $t('MSG_PURCHASE_AMOUNT') }}</h4>
+            <Input
+              v-model:value='myPurchaseAmount'
+              type='number'
+              id='amount'
+              required
+              :error='purchaseAmountError'
+              :message='$t("MSG_AMOUNT_TIP", {MAX: total})'
+              placeholder='MSG_AMOUNT_PLACEHOLDER'
+              :min='1'
+              :max='total'
+              @focus='onPurchaseAmountFocusIn'
+              @blur='onPurchaseAmountFocusOut'
+            />
+            <h4>{{ $t('MSG_PAYMENT_METHOD') }}</h4>
+            <CoinSelector
+              v-model:id='selectedCoinID'
+              :coins='coins'
+              label=''
+              hide-label
+              default
+              :tip-index='1'
+              :name-index='1'
+            />
+            <div class='warning' v-if='showRateTip'>
+              <img :src='warning'>
+              <span>{{ $t('MSG_COIN_USDT_EXCHANGE_RATE_TIP', { COIN_NAME: paymentCoin?.Unit }) }}</span>
+            </div>
+            <div class='warning' v-if='showBUSDTip'>
+              <img :src='warning'>
+              <span>{{ $t('MSG_COIN_BUSD_PAYMENT_TIP') }}</span>
+            </div>
+            <div class='submit-container'>
+              <WaitingBtn
+                label='MSG_PURCHASE'
+                type='submit'
+                class='submit-btn'
+                :disabled='submitting || !target?.EnablePurchase || !good.canBuy(undefined, target?.ID) || good.purchaseLimit(undefined, target?.ID) <= 0'
+                :waiting='submitting'
+                @click='onPurchaseClick'
+              />
+            </div>
+          </form>
+        </div>
+        <slot name='sidebar' />
+      </div>
+    </div>
+    <div class='hr' />
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { defineAsyncComponent, defineProps, toRef, ref, computed, onMounted, onUnmounted, watch } from 'vue'
+import { useRouter } from 'vue-router'
+import { getCoins } from 'src/api/chain'
+import { constant, appgood, appcoin, ledger, user } from 'src/npoolstore'
+
+import warning from 'src/assets/warning.svg'
+
+const CoinSelector = defineAsyncComponent(() => import('src/components/coin/CoinSelector.vue'))
+const WaitingBtn = defineAsyncComponent(() => import('src/components/button/WaitingBtn.vue'))
+// const BackPage = defineAsyncComponent(() => import('src/components/page/BackPage.vue'))
+const Input = defineAsyncComponent(() => import('src/components/input/Input.vue'))
+
+interface Props {
+  appGoodID: string
+  projectClass: string
+  bgImg: string
+  customizeInfo?: boolean
+  purchaseAmount?: number
+}
+
+const props = defineProps<Props>()
+const appGoodID = toRef(props, 'appGoodID')
+const projectClass = toRef(props, 'projectClass')
+const bgImg = toRef(props, 'bgImg')
+const customizeInfo = toRef(props, 'customizeInfo')
+const purchaseAmount = toRef(props, 'purchaseAmount')
+
+const router = useRouter()
+
+const logined = user.useLocalUserStore()
+const general = ledger.useLedgerStore()
+const good = appgood.useAppGoodStore()
+const target = computed(() => good.good(undefined, appGoodID.value))
+const total = computed(() => good.purchaseLimit(undefined, target.value?.ID as string))
+
+const coin = appcoin.useAppCoinStore()
+const coins = computed(() => coin.payableCoins().filter((el) => el.ENV === target.value?.CoinEnv))
+
+const defaultCoinTypeID = computed(() => {
+  return coins.value?.length > 0 ? coins.value?.[0].CoinTypeID : undefined as unknown as string
+})
+const selectedCoinID = ref(defaultCoinTypeID.value)
+const paymentCoin = computed(() => coin.coin(undefined, selectedCoinID.value))
+
+const showRateTip = computed(() => {
+  return paymentCoin.value?.Unit?.length &&
+        !paymentCoin.value?.Unit?.includes(constant.PriceCoinName) &&
+        !paymentCoin.value?.Name?.includes(constant.PriceCoinName) &&
+        !paymentCoin.value?.Unit?.includes('BUSD') &&
+        !paymentCoin.value?.Unit?.includes('USDC')
+})
+
+const showBUSDTip = computed(() => {
+  return paymentCoin.value?.Unit?.includes('BUSD')
+})
+
+const myPurchaseAmount = ref(purchaseAmount.value ? purchaseAmount.value : 1)
+const purchaseAmountError = ref(false)
+const onPurchaseAmountFocusIn = () => {
+  purchaseAmountError.value = false
+}
+const onPurchaseAmountFocusOut = () => {
+  purchaseAmountError.value = myPurchaseAmount.value <= 0 || myPurchaseAmount.value > total.value
+}
+
+const submitting = ref(false)
+
+const onPurchaseClick = () => {
+  if (!logined.logined) {
+    void router.push({
+      path: '/signin',
+      query: {
+        target: '/product/aleo',
+        appGoodID: target.value?.ID,
+        purchaseAmount: myPurchaseAmount.value
+      }
+    })
+    return
+  }
+  onPurchaseAmountFocusOut()
+  if (purchaseAmountError.value) {
+    return
+  }
+  general.$reset()
+  void router.push({
+    path: '/payment',
+    query: {
+      appGoodID: target.value?.ID,
+      coinTypeID: selectedCoinID.value,
+      purchaseAmount: myPurchaseAmount.value
+    }
+  })
+}
+
+const goIndexPage = () => {
+  if (!target.value?.EnableProductPage) {
+    void router.push({ path: '/' })
+  }
+}
+
+const endTime = computed(() => target.value?.SaleEndAt)
+const ticker = ref(-1)
+const remainDays = ref(27)
+const remainHours = ref(23)
+const remainMinutes = ref(59)
+const remainSeconds = ref(59)
+
+const showMe = ref(false)
+
+watch(target, () => {
+  if (!target.value) {
+    return
+  }
+  if (target.value && !target.value.EnableProductPage) {
+    goIndexPage()
+    return
+  }
+
+  showMe.value = true
+})
+
+onMounted(() => {
+  if (!coin.coins(undefined).length) {
+    getCoins(0, 100)
+  }
+
+  if (target.value && !target.value.EnableProductPage) {
+    goIndexPage()
+    return
+  }
+
+  if (target.value && target.value.EnableProductPage) {
+    showMe.value = true
+  }
+
+  ticker.value = window.setInterval(() => {
+    const now = Math.floor(Date.now() / 1000)
+    const remain = endTime.value as number - now >= 0 ? endTime.value as number - now : 0
+    remainDays.value = Math.floor(remain / 24 / 60 / 60)
+    remainHours.value = Math.floor((remain - remainDays.value * 24 * 60 * 60) / 60 / 60)
+    remainMinutes.value = Math.floor((remain - remainDays.value * 24 * 60 * 60 - remainHours.value * 60 * 60) / 60)
+    remainSeconds.value = remain - remainDays.value * 24 * 60 * 60 - remainHours.value * 60 * 60 - remainMinutes.value * 60
+    if (remainDays.value > 99) {
+      remainDays.value = 99
+    }
+  }, 1000)
+})
+
+onUnmounted(() => {
+  if (ticker.value >= 0) {
+    window.clearInterval(ticker.value)
+  }
+})
+</script>
+
+<style lang='sass' scoped>
+</style>
