1a2f6b5aafce2e8b204a3b1ca1b36992df9ec783
lidaqiang <dqli161@163.com>
2023-11-16 10:46:36 +0800

    add new router for new aleo-silver product page

---
+++ src/pages/product/AleoSilver2.vue
@@ -0,0 +1,10 @@
+<template>
+  <AleoSilver />
+</template>
+
+<script setup lang='ts'>
+import { defineAsyncComponent } from 'vue'
+
+const AleoSilver = defineAsyncComponent(() => import('src/components/aleo-silver2/AleoSilver.vue'))
+
+</script>
