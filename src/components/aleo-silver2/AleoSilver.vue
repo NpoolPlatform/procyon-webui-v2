fad23fd0fa78a61ab60fbba4fc0ff2a4e2711b73
Zhao KK <kikakkz@hotmail.com>
2023-11-18 17:19:46 +0800

    Correct style

--- src/components/aleo-silver2/AleoSilver.vue
+++ src/components/aleo-silver2/AleoSilver.vue
@@ -53,9 +53,12 @@
       </div>
       <div class='three-section'>
         <h4>{{ $t('MSG_ORDER_EFFECTIVE') }}:</h4>
-        <span class='number'>{{ target?.ServiceStartAt === 0 ? 'TBD*' : utils.formatTime(target?.ServiceStartAt as number, 'YYYY-MM-DD') }}</span>
-        <br>
-        <!-- <span class='unit'>{{ utils.formatTime(target?.ServiceStartAt as number, 'YYYY-MM-DD') }} {{ $t("MSG_JST") }}</span> -->
+        <span v-if='target?.ServiceStartAt === 0 || target?.StartMode === appgood.GoodStartMode.GoodStartModeTBD' class='number'>TBA</span>
+        <div v-else>
+          <span class='number'>{{ utils.formatTime(target?.ServiceStartAt as number, 'YYYY-MM-DD') }}</span>
+          <br>
+          <span class='unit'>{{ utils.formatTime(target?.ServiceStartAt as number, 'HH:mm:ss') }} {{ $t("MSG_JST") }}</span>
+        </div>
         <div class='tooltip'>
           <img class='more-info' :src='question'><span>{{ $t('MSG_LEARN_MORE1') }}</span>
           <p class='tooltip-text'>
