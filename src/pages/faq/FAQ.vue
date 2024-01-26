a362402ec5819cd3c1748986c953fe3bf5bf1abc
lidaqiang <dqli161@163.com>
2024-01-26 11:39:32 +0800

    fix faq jump

--- src/pages/faq/FAQ.vue
+++ src/pages/faq/FAQ.vue
@@ -5,7 +5,7 @@
 <script setup lang='ts'>
 import { defineAsyncComponent, watch, ref, Component, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 const { locale } = useI18n({ useScope: 'global' })
 
@@ -25,6 +25,16 @@ watch(locale, () => {
 onMounted(() => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/restrict-template-expressions
   FAQ.value = defineAsyncComponent(() => import(`src/pages/faq/${locale.value}/${topic.value}.vue`))
+})
+
+const router = useRouter()
+watch(router.currentRoute, () => {
+  const path = router.currentRoute.value.path
+  if (path?.startsWith('/faq')) {
+    const _topic = router.currentRoute.value.query.topic
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/restrict-template-expressions
+    FAQ.value = defineAsyncComponent(() => import(`src/pages/faq/${locale.value}/${_topic}.vue`))
+  }
 })
 
 </script>
