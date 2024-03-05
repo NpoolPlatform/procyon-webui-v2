036307597e908707cdf549ca8b5bf642aa7d77b3
Zhao KK <kikakkz@hotmail.com>
2024-03-05 15:39:13 +0800

    Add company info

--- src/pages/Company.vue
+++ src/pages/Company.vue
@@ -3,31 +3,29 @@
 </template>
 
 <script setup lang='ts'>
-import { computed, onMounted, ref } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { scrollTo } from 'src/utils/scroll'
-import { useRouter } from 'vue-router'
 import { article } from 'src/npoolstore'
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const router = useRouter()
 
 const { locale } = useI18n({ useScope: 'global' })
 const lang = computed(() => locale.value as string)
 const content = ref('')
 
-const contentUrl = `company/${lang.value}/company_info.html`
+const contentUrl = computed(() => `company/${lang.value}/company_info.html`)
 
 const _article = article.useArticleStore()
 const getContent = () => {
   _article.getContent({
-    ContentURL: contentUrl
+    ContentURL: contentUrl.value
   }, (error:boolean, str: string) => {
     if (error) return
     content.value = str
   })
 }
+
+watch(contentUrl, () => {
+  getContent()
+})
 
 onMounted(() => {
   getContent()
