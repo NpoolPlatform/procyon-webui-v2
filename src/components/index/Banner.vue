141684f5254e426637ee58ae5e293ea54f311ccc
EntropyPool <69444208+EntropyPool@users.noreply.github.com>
2024-04-01 19:45:15 +0800

    Update Banner.vue

--- src/components/index/Banner.vue
+++ src/components/index/Banner.vue
@@ -16,7 +16,7 @@
         class='hero-image' src='product/btc/bitcoin-banner.png'
       >
     </div>
-    <div class='content'>
+    <div v-if='false' class='content'>
       <div class='logo-feature'>
         <h3 class='logo-feature-title'>
           <span v-html='$t("MSG_ALEO_BACKER")' />
