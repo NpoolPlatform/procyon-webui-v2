04a2b48ee0cd0a1a393a9dd0401808aaece8c222
lidaqiang <dqli161@163.com>
2024-04-01 17:42:02 +0800

    delete comment

--- src/components/btc/Btc.vue
+++ src/components/btc/Btc.vue
@@ -2,15 +2,6 @@
   <div class='content'>
     <div class='product-container content-glass project' v-html='content' />
   </div>
-  <!-- <ProductPage
-    :app-good-i-d='(undefined as unknown as string)'
-    :purchase-amount='0'
-    project-class='project-aleo'
-    bg-img='product/btc/btc-banner.jpg'
-    :customize-info='true'
-  >
-    <div v-html='content' />
-  </ProductPage> -->
 </template>
 
 <script setup lang='ts'>
@@ -21,8 +12,6 @@ import { article } from 'src/npoolstore'
 const { locale } = useI18n({ useScope: 'global' })
 const lang = computed(() => locale.value as string)
 const content = ref('')
-
-// const ProductPage = defineAsyncComponent(() => import('src/components/product/ProductPage.vue'))
 
 const contentUrl = computed(() => `product/${lang.value}/btc_mining.html`)
 
@@ -43,4 +32,7 @@ watch(contentUrl, () => {
 onMounted(() => {
   getContent()
 })
+
 </script>
+<style lang='sass' scoped>
+</style>
